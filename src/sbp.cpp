3a16f71e14a62f53ace044b7f560d98754cd9efc
Alex Valavanis <valavanisalex@gmail.com>
2014-02-25 13:50:32 +0000

    sbp: src format (no change)

--- src/sbp.cpp
+++ src/sbp.cpp
@@ -28,8 +28,8 @@ using namespace constants;
 
 typedef
 struct	{
- double	E;		    /* total electron energy		  */
- double	SR;		    /* scattering rate            	  */
+    double	E;		    /* total electron energy		  */
+    double	SR;		    /* scattering rate            	  */
 } data;
 
 double   f(double E_F, double Emax, double Emin, double m, int N, double T);
@@ -66,11 +66,11 @@ class SBPOptions : public Options
                     ;
 
                 std::string doc("Find the Fermi-Dirac distribution function for an individual "
-                                "subband given its population and temperature."
-                                "The subband minimum is read from the file \"E*.r\", "
-                                "and the distribution is written to \"FDi.r\" where the '*' "
-                                "is replaced by the particle ID in each case and the "
-                                "'i' is replaced by the number of the state");
+                        "subband given its population and temperature."
+                        "The subband minimum is read from the file \"E*.r\", "
+                        "and the distribution is written to \"FDi.r\" where the '*' "
+                        "is replaced by the particle ID in each case and the "
+                        "'i' is replaced by the number of the state");
 
                 add_prog_specific_options_and_parse(argc, argv, doc);	
             }
@@ -101,33 +101,33 @@ int main(int argc,char *argv[])
 {
     SBPOptions opt(argc, argv);
 
-FILE	*FEf;			/* file pointer to Fermi Energy file	*/
+    FILE	*FEf;			/* file pointer to Fermi Energy file	*/
 
-const bool   FD_flag = opt.get_fd_flag();
-const double m       = opt.get_mass();
-const size_t nE      = opt.get_n_energy();
-const char   p       = opt.get_particle();
-const double T       = opt.get_temperature();
+    const bool   FD_flag = opt.get_fd_flag();
+    const double m       = opt.get_mass();
+    const size_t nE      = opt.get_n_energy();
+    const char   p       = opt.get_particle();
+    const double T       = opt.get_temperature();
 
-std::valarray<double> E=read_E(p); // reads subband energy file
-const size_t n = E.size();
-double *N=read_populations(n);		/* reads subband populations file	*/
+    std::valarray<double> E=read_E(p); // reads subband energy file
+    const size_t n = E.size();
+    double *N=read_populations(n);		/* reads subband populations file	*/
 
-if((FEf=fopen("Ef.r","w"))==0)
- {fprintf(stderr,"Error: Cannot open input file 'Ef.r'!\n");exit(0);}
+    if((FEf=fopen("Ef.r","w"))==0)
+    {fprintf(stderr,"Error: Cannot open input file 'Ef.r'!\n");exit(0);}
 
-for(unsigned int s=0; s<n; s++) // s=0 => ground state
-{
- const double Ef=calc_fermilevel(E[s],m,*(N+s),T);
+    for(unsigned int s=0; s<n; s++) // s=0 => ground state
+    {
+        const double Ef=calc_fermilevel(E[s],m,*(N+s),T);
 
- fprintf(FEf,"%i %20.17le\n",s+1,Ef/(1e-3*e));
+        fprintf(FEf,"%i %20.17le\n",s+1,Ef/(1e-3*e));
 
- if(FD_flag) calc_dist(E[s],Ef,m,T,nE,s);
-}
+        if(FD_flag) calc_dist(E[s],Ef,m,T,nE,s);
+    }
 
-fclose(FEf);
+    fclose(FEf);
 
-return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 } /* end main */
 
 /**
@@ -142,48 +142,48 @@ return EXIT_SUCCESS;
  */
 void calc_dist(double Emin, double Ef, double m, double T, int nE, int s)
 {
-double	dE;			/* energy increment in integration	*/
-double	E;			/* Energy				*/
-double	Emax;
-double	f;			/* probability of occupation		*/
-double	Ne=0;			/* total electron density (for checking)*/
-double	vmax;			/* maximum value of potential		*/
-int     i;			/* general index			*/
-char	filename[9];		/* output filename for FD distribs.	*/
-FILE	*FFD;			/* file pointer to Fermi-Dirac probability
-				   of occupation of subband file	*/
+    double	dE;			/* energy increment in integration	*/
+    double	E;			/* Energy				*/
+    double	Emax;
+    double	f;			/* probability of occupation		*/
+    double	Ne=0;			/* total electron density (for checking)*/
+    double	vmax;			/* maximum value of potential		*/
+    int     i;			/* general index			*/
+    char	filename[9];		/* output filename for FD distribs.	*/
+    FILE	*FFD;			/* file pointer to Fermi-Dirac probability
+                                           of occupation of subband file	*/
 
-sprintf(filename,"FD%i.r",s+1);
-FFD=fopen(filename,"w");
+    sprintf(filename,"FD%i.r",s+1);
+    FFD=fopen(filename,"w");
 
-vmax=Vmax();
-Emax=Ef+10*kB*T;if(Emax>vmax) Emax=vmax;
+    vmax=Vmax();
+    Emax=Ef+10*kB*T;if(Emax>vmax) Emax=vmax;
 
-/* Implement trapezium rule integration, i.e. `ends+2middles' 	*/
+    /* Implement trapezium rule integration, i.e. `ends+2middles' 	*/
 
-f=1/(exp((Emin-Ef)/(kB*T))+1);
-fprintf(FFD,"%20.17le %20.17le\n",Emin/(1e-3*e),f);
-Ne+=f;
+    f=1/(exp((Emin-Ef)/(kB*T))+1);
+    fprintf(FFD,"%20.17le %20.17le\n",Emin/(1e-3*e),f);
+    Ne+=f;
 
-dE=(Emax-Emin)/(nE-1);
-E=Emin;
-for(i=1;i<nE-1;i++)
-{
- E+=dE;
- f=1/(exp((E-Ef)/(kB*T))+1);
- fprintf(FFD,"%20.17le %20.17le\n",E/(1e-3*e),f);
- Ne+=2*f;
-}
+    dE=(Emax-Emin)/(nE-1);
+    E=Emin;
+    for(i=1;i<nE-1;i++)
+    {
+        E+=dE;
+        f=1/(exp((E-Ef)/(kB*T))+1);
+        fprintf(FFD,"%20.17le %20.17le\n",E/(1e-3*e),f);
+        Ne+=2*f;
+    }
 
-f=1/(exp((Emax-Ef)/(kB*T))+1);
-fprintf(FFD,"%20.17le %20.17le\n",Emax/(1e-3*e),f);
-Ne+=f;
+    f=1/(exp((Emax-Ef)/(kB*T))+1);
+    fprintf(FFD,"%20.17le %20.17le\n",Emax/(1e-3*e),f);
+    Ne+=f;
 
-Ne*=m/(pi*gsl_pow_2(hBar))*0.5*dE;
+    Ne*=m/(pi*gsl_pow_2(hBar))*0.5*dE;
 
-printf("Ne=%20.17le\n",Ne/1e+14);
+    printf("Ne=%20.17le\n",Ne/1e+14);
 
-fclose(FFD);
+    fclose(FFD);
 }
 
 /**
@@ -210,40 +210,40 @@ fclose(FFD);
  */
 double calc_fermilevel(double E, double m, double N, double T)
 {
-double	delta_E=0.001*1e-3*e;	/* energy increment			*/
-double 	Emin;			/* subband minimum			*/
-double 	Emax;			/* subband maximum			*/
-double	vmax;			/* potential maximum, i.e. top of well	*/
-double	x;			/* independent variable			*/
-double	y1;			/* dependent variable			*/
-double	y2;			/* dependent variable			*/
+    double	delta_E=0.001*1e-3*e;	/* energy increment			*/
+    double 	Emin;			/* subband minimum			*/
+    double 	Emax;			/* subband maximum			*/
+    double	vmax;			/* potential maximum, i.e. top of well	*/
+    double	x;			/* independent variable			*/
+    double	y1;			/* dependent variable			*/
+    double	y2;			/* dependent variable			*/
 
-Emin=E;				/* subband minimum 			*/
+    Emin=E;				/* subband minimum 			*/
 
-vmax=Vmax();			/* calulate potential maximum		*/
+    vmax=Vmax();			/* calulate potential maximum		*/
 
-x=Emin-20*kB*T;			/* first value of x			*/
+    x=Emin-20*kB*T;			/* first value of x			*/
 
-/* In this implementation, the upper limit of integration is set at the 
-   Fermi level+10kT, limited at potential maximum			*/
+    /* In this implementation, the upper limit of integration is set at the 
+       Fermi level+10kT, limited at potential maximum			*/
 
-Emax=x+10*kB*T;if(Emax>vmax) Emax=vmax;
+    Emax=x+10*kB*T;if(Emax>vmax) Emax=vmax;
 
-y2=f(x,Emax,Emin,m,N,T);
+    y2=f(x,Emax,Emin,m,N,T);
 
-do
-{
- y1=y2;
- x+=delta_E;
- Emax=x+10*kB*T;if(Emax>vmax) Emax=vmax;
- y2=f(x,Emax,Emin,m,N,T);
-}while(y1*y2>0);
+    do
+    {
+        y1=y2;
+        x+=delta_E;
+        Emax=x+10*kB*T;if(Emax>vmax) Emax=vmax;
+        y2=f(x,Emax,Emin,m,N,T);
+    }while(y1*y2>0);
 
-/* improve estimate using midpoint rule */
+    /* improve estimate using midpoint rule */
 
-x-=fabs(y2)/(fabs(y1)+fabs(y2))*delta_E;
+    x-=fabs(y2)/(fabs(y1)+fabs(y2))*delta_E;
 
-return(x);
+    return(x);
 }
 
 /**
@@ -258,16 +258,16 @@ return(x);
  */
 double f(double E_F, double Emax, double Emin, double m, int N, double T)
 {
-double	y;			/* dependent variable			*/
+    double	y;			/* dependent variable			*/
 
-y=m/(pi*hBar)*(kB*T/hBar)*
-  (
-   ((Emax-E_F)/(kB*T)-log(1+exp((Emax-E_F)/(kB*T))))-
-   ((Emin-E_F)/(kB*T)-log(1+exp((Emin-E_F)/(kB*T))))
-  )
-  -N;
+    y=m/(pi*hBar)*(kB*T/hBar)*
+        (
+         ((Emax-E_F)/(kB*T)-log(1+exp((Emax-E_F)/(kB*T))))-
+         ((Emin-E_F)/(kB*T)-log(1+exp((Emin-E_F)/(kB*T))))
+        )
+        -N;
 
-return(y);
+    return(y);
 }
 
 /**
@@ -278,40 +278,40 @@ return(y);
  */
 double * read_populations(int n)
 {
- double	*N;
- int	i=0;		/* index over the energies			*/
- int	m;		/* counter over number of populations defined	*/
- FILE 	*FN;		/* file pointer to energy data 			*/
+    double	*N;
+    int	i=0;		/* index over the energies			*/
+    int	m;		/* counter over number of populations defined	*/
+    FILE 	*FN;		/* file pointer to energy data 			*/
 
- if((FN=fopen("N.r","r"))==0)
- {
-   fprintf(stderr,"Error: Cannot open input file 'N.r'!\n");
-   exit(0);
- }
+    if((FN=fopen("N.r","r"))==0)
+    {
+        fprintf(stderr,"Error: Cannot open input file 'N.r'!\n");
+        exit(0);
+    }
 
- m=0;
- while(fscanf(FN,"%*i %*f")!=EOF)
-  m++;
- rewind(FN);
+    m=0;
+    while(fscanf(FN,"%*i %*f")!=EOF)
+        m++;
+    rewind(FN);
 
- if(m!=n)
-  {printf("Subband populations not defined for all levels!\n");exit(0);}
+    if(m!=n)
+    {printf("Subband populations not defined for all levels!\n");exit(0);}
 
- N=(double *)calloc(n,sizeof(double));
- if (N==0)  {
-  fprintf(stderr,"Cannot allocate memory!\n");
-  exit(0);
- }
+    N=(double *)calloc(n,sizeof(double));
+    if (N==0)  {
+        fprintf(stderr,"Cannot allocate memory!\n");
+        exit(0);
+    }
 
- while(fscanf(FN,"%*i %lf",N+i)!=EOF)
- {
-  *(N+i)*=1e+10*1e+4;	/*convert from units of 10^10cm^-2->m^-2 */
-  i++;
- }
+    while(fscanf(FN,"%*i %lf",N+i)!=EOF)
+    {
+        *(N+i)*=1e+10*1e+4;	/*convert from units of 10^10cm^-2->m^-2 */
+        i++;
+    }
 
- fclose(FN);
+    fclose(FN);
 
- return(N);
+    return(N);
 
 }
 
@@ -320,21 +320,21 @@ double * read_populations(int n)
  */
 double Vmax()
 {
- double	max;			/* maximum value of potential energy	*/
- double	v;			/* potential				*/
- FILE	*Fv;			/* file pointer to v.r			*/
+    double	max;			/* maximum value of potential energy	*/
+    double	v;			/* potential				*/
+    FILE	*Fv;			/* file pointer to v.r			*/
 
-max=0;
+    max=0;
 
-if((Fv=fopen("v.r","r"))==0)
- {fprintf(stderr,"Error: Cannot open input file 'v.r'!\n");exit(0);}
+    if((Fv=fopen("v.r","r"))==0)
+    {fprintf(stderr,"Error: Cannot open input file 'v.r'!\n");exit(0);}
 
-while(fscanf(Fv,"%*e %le",&v)!=EOF)
- if(v>max) max=v;
+    while(fscanf(Fv,"%*e %le",&v)!=EOF)
+        if(v>max) max=v;
 
-fclose(Fv);
+    fclose(Fv);
 
-return(max);
+    return(max);
 
 }
 // vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99 :
