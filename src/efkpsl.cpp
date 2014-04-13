409373633a81dd4c704a34e8ea0e3f82787f2226
Alex Valavanis <valavanisalex@gmail.com>
2014-04-13 16:34:07 +0100

    efkpsl: Use GSL for search to speed things up

--- src/efkpsl.cpp
+++ src/efkpsl.cpp
@@ -32,186 +32,246 @@
  The first estimate of the energy is found by 
  following the function along the x axis until it
  changes sign then using a midpoint rule.
-
- Paul Harrison, April 1998
  */
 
 #include <cstdio>
 #include <cstdlib>
+#include <gsl/gsl_errno.h>
 #include <gsl/gsl_math.h>
+#include <gsl/gsl_roots.h>
+#include <valarray>
+#include "qwwad-options.h"
 #include "qclsim-constants.h"
+#include "qclsim-fileio.h"
 
 using namespace Leeds;
 using namespace constants;
 
-static double f(const double a,
-                const double b,
-                const double energy,
-                const double k,
-                const double m_b,
-                const double m_w,
-                const double V);
+/// Parameters needed for computing superlattice eigenstates
+struct kpsl_params
+{
+    double a;           ///< Well width [m]
+    double b;           ///< Barrier width [m]
+    double m_w;         ///< Well mass [kg]
+    double m_b;         ///< Barrier mass [kg]
+    double V;           ///< Barrier potential [J]
+    double k;           ///< Wave vector [1/m]
+};
+
+static double f(double  energy,
+                void   *params);
+
+/**
+ * Handler for command-line options
+ */
+class EFKPSLOptions : public Options
+{
+    public:
+        EFKPSLOptions(int argc, char* argv[])
+        {
+            try
+            {
+                program_specific_options->add_options()
+                    ("well-width,a", po::value<double>()->default_value(100),
+                     "Width of quantum well [angstrom].")
+                    
+                    ("barrier-width,b", po::value<double>()->default_value(100),
+                     "Width of barrier [angstrom].")
+
+                    ("wave-vector,k", po::value<double>()->default_value(0),
+                     "Wave-vector expressed relative to pi/L, where L is "
+                     "the period length")
+
+                    ("well-mass,m", po::value<double>()->default_value(0.067),
+                     "Effective mass in well (relative to that of a free electron)")
+
+                    ("barrier-mass,n", po::value<double>()->default_value(0.067),
+                     "Effective mass in barrier (relative to that of a free electron)")
+
+//                    ("output-equations", po::bool_switch()->default_value(false),
+//                     "Output the matching equations for the system")
+
+                    ("particle,p", po::value<char>()->default_value('e'),
+                     "Particle to be used: 'e', 'h' or 'l'")
+
+                    ("states,s", po::value<size_t>()->default_value(1),
+                     "Number of states to find")
+
+                    ("potential", po::value<double>()->default_value(100),
+                     "Barrier potential [meV]")
+                    ;
+
+                std::string doc("Find the eigenstate of an infinite Kronig-Penney superlattice "
+                                "that corresponds to the specified wave-vector. "
+                                "The energies are written to the file \"E*.r\", "
+//                                "and the "
+//                                "wavefunctions are written to \"wf_*i.r\" "
+                                "where the '*' "
+                                "is replaced by the particle ID in each case "
+//                                "and the "
+//                                "'i' is replaced by the number of the state"
+                        );
+
+                add_prog_specific_options_and_parse(argc, argv, doc);	
+            }
+            catch(std::exception &e)
+            {
+                std::cerr << e.what() << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        /// \returns the width of the quantum well [m]
+        double get_well_width() const {return vm["well-width"].as<double>()*1e-10;}
+        
+        /// \returns the width of the barriers [m]
+        double get_barrier_width() const {return vm["barrier-width"].as<double>()*1e-10;}
+        
+        /// \returns the wave vector [1/m]
+        double get_wave_vector() const {
+            const double k = vm["wave-vector"].as<double>();
+            const double a = get_well_width();
+            const double b = get_barrier_width();
+            const double L = a + b; // Total period length [m]
+
+            return k * pi/L;
+        }
+
+//        /// \returns true if we are required to output the matching equations for the system
+//        bool output_equations() const {return vm["output-equations"].as<bool>();}
+
+        /// \returns the effective mass in the quantum well [kg]
+        double get_well_mass() const {return vm["well-mass"].as<double>()*me;}
+
+        /// \returns the effective mass in the quantum well [kg]
+        double get_barrier_mass() const {return vm["barrier-mass"].as<double>()*me;}
+
+        /// \returns the particle ID
+        char get_particle() const {return vm["particle"].as<char>();}
+
+        /// \returns the number of spatial points
+        size_t get_n_states() const {return vm["states"].as<size_t>();}
+
+        /// \returns the effective mass in the quantum well [J]
+        double get_potential() const {return vm["potential"].as<double>()*1e-3*e;}
+};
 
 int main(int argc,char *argv[])
 {
-    double	a;		/* well width				*/
-    double	b;		/* barrier width			*/
-    double	dy;		/* derivative of y			*/
-    double	E;		/* uncorrelated energies		*/
-    double	dx;		/* small energy increment		*/
-    double	k;		/* wave vector				*/
-    double	m_w;		/* effective masses in the well		*/
-    double	m_b;		/* effective masses in the barrier	*/
-    double	V;		/* potential				*/
-    double	x;		/* energy estimate			*/
-    double	y;		/* current functional value		*/
-    double	y1;		/* temporary y value			*/
-    double	y2;		/*     "     "   " 			*/
-    int	i_state;	/* principal quantum number index	*/
-    int	state;		/* number of states			*/
-    char	p;		/* particle				*/
-    char	filename[9];	/* output filename			*/
-    FILE	*FE;		/* pointer to energy file 		*/
+    const EFKPSLOptions opt(argc, argv);
 
-    /* default values, appropriate to GaAs-GaAlAs */
+    const double a   = opt.get_well_width();    // [m]
+    const double b   = opt.get_barrier_width(); // [m]
+    const double k   = opt.get_wave_vector();   // [1/m]
+    const double m_w = opt.get_well_mass();     // [kg]
+    const double m_b = opt.get_barrier_mass();  // [kg]
+    const double V   = opt.get_potential();     // [J]
+    const size_t nst = opt.get_n_states();
+    const char   p   = opt.get_particle();
 
-    a=100e-10;          
-    b=100e-10;
-    k=0;
-    m_w=0.067*me;
-    m_b=0.067*me;
-    p='e';
-    V=100*1e-3*e;   
-    state=1;
+    kpsl_params params = {a, b, m_w, m_b, V, k};
+    gsl_function F;
+    F.function = &f;
+    F.params   = &params;
+    gsl_root_fsolver *solver = gsl_root_fsolver_alloc(gsl_root_fsolver_brent);
 
-    /* Computational values	*/
+    const double dx=1e-3*e; // arbitrarily small energy increment---0.1meV
 
-    dx=1e-3*e;        /* arbitrarily small energy---0.1meV   */
+    double Elo=dx;    // first energy estimate
+    std::valarray<double> E(nst);
 
-    x=dx;    /* first energy estimate */
-
-    while((argc>1)&&(argv[1][0]=='-'))
+    for(unsigned int ist=0; ist<nst; ++ist)
     {
-        switch(argv[1][1])
+        // Shift the lower estimate up past the last state we found
+        if(ist > 0) Elo = E[ist-1] + dx;
+
+        // Value for y=f(x) at bottom of search range
+        const double y1 = GSL_FN_EVAL(&F,Elo);
+
+        // Find the range in which the solution lies by incrementing the
+        // upper limit of the search range until the function changes sign.
+        // Since this coarse search can require many iterations, we keep the
+        // lower limit fixed to minimise the amount of computation at each step.
+        // The Brent algorithm is extremely fast, so it really doesn't matter that
+        // the range we find here is large.
+        //
+        // Note the end stop to prevent infinite loop in absence of solution
+        //
+        // TODO: Make the cut-off configurable
+        double y2 = y1;
+        double Ehi = Elo;
+        do
         {
-            case 'a':
-                a=atof(argv[2])*1e-10;
-                break;
-            case 'b':
-                b=atof(argv[2])*1e-10;
-                break;
-            case 'k':
-                k=atof(argv[2]);
-                break;
-            case 'm':
-                m_w=atof(argv[2])*me;
-                break;
-            case 'n':
-                m_b=atof(argv[2])*me;
-                break;
-            case 'p':
-                p=*argv[2];
-                switch(p)
-                {
-                    case 'e': break;
-                    case 'h': break;
-                    case 'l': break;
-                    default:  printf("Usage:  efkpsl [-p particle (\033[1me\033[0m, h, or l)]\n");
-                              exit(0);
-                }
-                break;
-            case 's':
-                state=atoi(argv[2]);
-                break;
-            case 'V':
-                V=atof(argv[2])*1e-3*e;
-                break;
-            default:
-                printf("Usage:  efkpsl [-a well width (\033[1m100\033[0mA)][-b barrier width (\033[1m100\033[0mA)]\n");
-                printf("               [-k wave vector (\033[1m0\033[0m pi/L)\n");
-                printf("               [-m well mass (\033[1m0.067\033[0mm0)][-n barrier mass (\033[1m0.067\033[0mm0)\n");
-                printf("               [-p particle (\033[1me\033[0m, h, or l)][-s # states \033[1m1\033[0m]\n");
-                printf("               [-V barrier height (\033[1m100\033[0mmeV)]\n");
-                exit(EXIT_FAILURE);
-        }
-        argv++;
-        argv++;
-        argc--;
-        argc--;
+            Ehi+=dx;
+            y2=GSL_FN_EVAL(&F, Ehi);
+        }while((y1*y2>0)&&(Ehi<100*V));
+
+        E[ist] = (Elo + Ehi)/2;
+        gsl_root_fsolver_set(solver, &F, Elo, Ehi);
+        int status = 0;
+
+        // Improve the estimate of the solution using the Brent algorithm
+        // until we hit a desired level of precision
+        do
+        {
+            status = gsl_root_fsolver_iterate(solver);
+            E[ist] = gsl_root_fsolver_root(solver);
+            Elo = gsl_root_fsolver_x_lower(solver);
+            Ehi = gsl_root_fsolver_x_upper(solver);
+            status = gsl_root_test_interval(Elo, Ehi, 1e-9*e, 0);
+        }while(status == GSL_CONTINUE);
     }
 
-    /* Need to convert k into SI now sure of L (=a+b)	*/
-
-    k*=pi/(a+b);
-
+    // Output energies to file
+    char filename[9];
     sprintf(filename,"E%c.r",p);
-    FE=fopen(filename,"w");
-
-    for(i_state=1;i_state<=state;i_state++)
-    {
-        do      /* loop increments energy until function changes sign */
-        {
-            y1=f(a,b,x,k,m_b,m_w,V);
-            x+=dx;
-            y2=f(a,b,x,k,m_b,m_w,V);
-        }while((y1*y2>0)&&(x<10*V));
-
-        /* Note the end stop to prevent infinite loop in absence of solution	*/ 
-
-        x-=fabs(y2)/(fabs(y1)+fabs(y2))*dx;
-
-        do      /* Newton-Raphson iterative method */
-        {
-            y=f(a,b,x,k,m_b,m_w,V);
-            dy=(f(a,b,x+dx/100,k,m_b,m_w,V)-f(a,b,x-dx/100,k,m_b,m_w,V))/(2*dx/100);
-            x-=y/dy;
-        }while(fabs(y/dy)>1e-9*e);	
-
-        E=x;
-
-        fprintf(FE,"%i %20.17le\n",i_state,E/(1e-3*e));
-
-        x+=dx;       /* clears x from solution */
-    }/* end i_state */
-
-    fclose(FE);
+    E *= 1e3/e; // Rescale to meV
+    Leeds::write_table_x(filename, E, true);
 
     return EXIT_SUCCESS;
-}        /* end main */
+}
 
 /**
  * \brief This function is the standard fw result = 0
  *
  * \param[in] energy Local energy
+ * \param[in] params Superlattice parameters (cast to kpsl_params)
  */
-static double f(const double a,
-                const double b,
-                const double energy,
-                const double k,
-                const double m_b,
-                const double m_w,
-                const double V)
+static double f(double  energy,
+                void   *params)
 {
-    double F;	    /* value of function	   */
-    double k_w;        /* wave vector in well         */
-    double k_b;        /* wave vector in barrier      */
-    double K;	    /* wave function decay const.  */
+    const kpsl_params *p = reinterpret_cast<kpsl_params *>(params);
+    const double a   = p->a;
+    const double b   = p->b;
+    const double m_b = p->m_b;
+    const double m_w = p->m_w;
+    const double V   = p->V;
+    const double k   = p->k;
+
+    double F; // value of function
+
+    // Wave vector inside well [QWWAD3, 2.155]
+    const double k_w=sqrt(2*m_w/hBar*energy/hBar);
 
     if(energy<V)
     {
-        k_w=sqrt(2*m_w/hBar*energy/hBar);
-        K=sqrt(2*m_b/hBar*(V-energy)/hBar);
+        // If we're below the barrier, then the wavefunction decays with
+        // [QWWAD3, 2.167]
+        const double K=sqrt(2*m_b/hBar*(V-energy)/hBar);
 
+        // Matching equation [QWWAD3, 2.169]
         F=cos(k_w*a)*cosh(K*b)-sin(k_w*a)*sinh(K*b)*
             (gsl_pow_2(m_b*k_w)-gsl_pow_2(m_w*K))/(2*m_w*m_b*k_w*K)-cos(k*(a+b));
     }
     else
     {
-        k_w=sqrt(2*m_w/hBar*energy/hBar);
-        k_b=sqrt(2*m_b/hBar*(energy-V)/hBar);
+        // If we're above the barrier, then the wavefunction propagates
+        // with [QWWAD3, 2.155]
+        const double k_b=sqrt(2*m_b/hBar*(energy-V)/hBar);
 
+        // Matching equation [QWWAD3, 2.166]
         F=cos(k_w*a)*cos(k_b*b)-sin(k_w*a)*sin(k_b*b)*
-            (gsl_pow_2(m_b*k_w)-gsl_pow_2(m_w*k_b))/(2*m_w*m_b*k_w*k_b)-cos(k*(a+b));
+            (gsl_pow_2(m_b*k_w)+gsl_pow_2(m_w*k_b))/(2*m_w*m_b*k_w*k_b)-cos(k*(a+b));
     }
 
     return F;
