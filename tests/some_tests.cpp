080f8717c281653ec42e18201a893077c78f20d9
Jonny Cooper <jdc.tas@gmail.com>
2014-04-23 18:12:20 +0100

    Added placeholder CMakeLists.txt files. The files don't currently build anything from the src dir due to incompatibilities with Mac OSX. There is an example of some tests using gtest in the tests folder which is build.

---
+++ tests/some_tests.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+class SomeTest : public ::testing::Test
+{
+};
+
+TEST_F( SomeTest, aPassingTest )
+{
+    EXPECT_EQ( 2, 1+1 );
+}
+
+TEST_F( SomeTest, aFailingTest )
+{
+    EXPECT_EQ( 3, 1+1 );
+}
