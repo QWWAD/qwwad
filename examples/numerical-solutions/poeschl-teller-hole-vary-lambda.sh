479ca71ee96e4fb156216c9a43d7accc1523d78e
Alex Valavanis <valavanisalex@gmail.com>
2014-06-21 22:55:07 +0100

    distribute poeschl-teller hole plot/table

--- examples/numerical-solutions/poeschl-teller-hole-vary-lambda.sh
+++ examples/numerical-solutions/poeschl-teller-hole-vary-lambda.sh
@@ -2,32 +2,56 @@
 set -e
 
 # Define output file
-OUT=E-LAMBDA.r
+outfile_E=poeschl-teller-hole-E-lambda.dat
+outfile_V=poeschl-teller-hole-V.dat
 
 # Initialise files
-
-rm -f Ee-LAMBDA*
-rm -f v-LAMBDA*
-rm -f $OUT
+rm -f $outfile_E $outfile_V
 
 # Define length of Poschl-Teller potential
 L=300
 
 # Loop over depth parameter lambda
-for LAMBDA in 0.01 0.02 0.05 0.1 0.2 0.5 0.75 1 1.5 2.0 5.0 10.0
-do
-{
- pth -a 0.05 -l $LAMBDA -L $L
- echo -n $LAMBDA >> $OUT
+for LAMBDA in 0.75 1 1.5 2.0 5.0 10.0; do
 
- # Now perform numerical solution
- efss --nst-max 2 --mass 0.067 --solver matrix-constant-mass	# calculate 2 lowest energy levels
- 
- # Save lowest two energies to file
- awk '/^1/||/^2/{printf("\t%f",$2)}' Ee.r >> $OUT	
- cp Ee.r Ee-LAMBDA=$LAMBDA.r
- 
- echo -n -e "\n" >> $OUT
- cp v.r v-LAMBDA=$LAMBDA.r
-}
+    # Find potential profile and get analytical solution
+    pth --alpha 0.05 --lambda $LAMBDA --length $L
+
+    E1_analytical=`awk '/^1/{print $2}' Ee.r`
+    E2_analytical=`awk '/^2/{print $2}' Ee.r`
+
+    # Fill in the blanks in the table if there is no solution
+    if [ x$E1_analytical = "x" ]; then
+        E1_analytical="--"
+    fi
+
+    if [ x$E2_analytical = "x" ]; then
+        E2_analytical="--"
+    fi
+
+    # Now perform numerical solution
+    efss --nst-max 2 --mass 0.067 --solver matrix-constant-mass	# calculate 2 lowest energy levels
+
+    E1_numerical=`awk '/^1/{print $2}' Ee.r`
+    E2_numerical=`awk '/^2/{print $2}' Ee.r`
+
+    if [ x$E1_numerical = "x" ]; then
+        E1_numerical="--"
+    fi
+
+    if [ x$E2_numerical = "x" ]; then
+        E2_numerical="--"
+    fi
+
+    printf "%e\t%s\t%s\t%s\t%s\n" $LAMBDA $E1_analytical $E2_analytical $E1_numerical $E2_numerical >> $outfile_E
+    cp v.r v-$LAMBDA.r
 done
+
+awk '{print $1*1e10, $2*1000/1.6e-19}' v-0.75.r >> $outfile_V
+printf "\n" >> $outfile_V
+awk '{print $1*1e10, $2*1000/1.6e-19}' v-1.r >> $outfile_V
+printf "\n" >> $outfile_V
+awk '{print $1*1e10, $2*1000/1.6e-19}' v-1.5.r >> $outfile_V
+printf "\n" >> $outfile_V
+awk '{print $1*1e10, $2*1000/1.6e-19}' v-2.0.r >> $outfile_V
+printf "\n" >> $outfile_V
--- src/pth.cpp
+++ src/pth.cpp
@@ -10,8 +10,12 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <valarray>
+
 #include <gsl/gsl_math.h>
+#include "qwwad-options.h"
 #include "qclsim-constants.h"
+#include "qclsim-fileio.h"
 
 using namespace Leeds;
 using namespace constants;
@@ -25,32 +29,29 @@ Options configure_options(int argc, char* argv[])
 
     opt.add_numeric_option("alpha,a",    0.1,   "Width parameter [1/angstrom].");
     opt.add_numeric_option("lambda,l",   2.0,   "Depth parameter.");
+    opt.add_numeric_option("length,l",   300,   "Length of potential profile [angstrom].");
     opt.add_numeric_option("mass,m",     0.067, "Effective mass (relative to free electron).");
-    opt.add_size_option   ("nz,N",       100,   "Number of spatial points for output file.");
-    opt.add_size_option   ("nst,s",      1,     "Number of states to find.");
+    opt.add_size_option   ("nz,N",       301,   "Number of spatial points for output file.");
     opt.add_char_option   ("particle,p", 'e',   "ID of particle to be used: 'e', 'h' or 'l', for electrons, heavy holes or light holes respectively.");
-    opt.add_numeric_option("vcb",        0.00,  "Band-edge potential [eV]");
-    opt.add_numeric_option("alpha",      0.00,  "Non-parabolicity parameter [eV^{-1}]");
-    opt.add_numeric_option("E-cutoff",          "Cut-off energy for solutions [meV]");
 
-    std::string doc("Generate a Poeschl--Teller potential profile.");
+    std::string doc("Generate a Poeschl--Teller potential profile and finds eigenstate energies analytically.");
 
     std::string details("The following output text files are created:\n"
                         "  'E*.r'   \tEnergy of each state:\n"
                         "           \tCOLUMN 1: state index.\n"
                         "           \tCOLUMN 2: energy [meV].\n"
-                        "  'wf_*i.r'\tWave function amplitude at each position\n"
+                        "  'v.r'    \tPotential at each position\n"
                         "           \tCOLUMN 1: position [m].\n"
-                        "           \tCOLUMN 2: wave function amplitude [m^{-1/2}].\n"
+                        "           \tCOLUMN 2: confining potential [J].\n"
                         "\n"
                         "\tIn each case, the '*' is replaced by the particle ID and the 'i' is replaced by the number of the state.\n"
                         "\n"
                         "Examples:\n"
-                        "   Compute the ground state in a 150-angstrom well with effective mass = 0.1 m0:\n\n"
-                        "   efiw --width 150 --mass 0.1\n"
+                        "   Compute the potential and state energies for Poeschl--Teller hole with depth parameter 2, and width parameter 0.05/angstrom:\n\n"
+                        "   pth --alpha 0.05 --lambda 2\n"
                         "\n"
-                        "   Compute the first three heavy-hole states in a 200-angstrom well, using effective mass = 0.62 m0:\n\n"
-                        "   efiw --width 200 --mass 0.62 --particle h");
+                        "   Compute solutions for a Poeschl--Teller hole with depth 2, width 0.1/angstrom and effective mass 0.1me:\n\n"
+                        "   pth --alpha 0.1 --mass 0.1 --lambda 2");
 
     opt.add_prog_specific_options_and_parse(argc, argv, doc, details);
 
@@ -61,103 +62,50 @@ int main(int argc,char *argv[])
 {
     const Options opt = configure_options(argc, argv);
 
-    const double alpha  = opt.get_numeric_option("alpha") * 1e10; // Width parameter [1/m]
-    const double lambda = opt.get_numeric_option("lambda");       // Depth parameter
+    const double alpha  = opt.get_numeric_option("alpha") * 1e10;   // Width parameter [1/m]
+    const double lambda = opt.get_numeric_option("lambda");         // Depth parameter
+    const double L      = opt.get_numeric_option("length") * 1e-10; // Length of potential [m]
+    const double m      = opt.get_numeric_option("mass") * me;      // effective mass [kg]
+    const size_t nz     = opt.get_size_option("nz"); // Number of spatial points for output file
+    const char   p      = opt.get_char_option("particle"); // Particle ID
 
-double	E;		/* energy				*/
-double	L;		/* length of potential			*/
-double	m;		/* effective mass			*/
-double	V;		/* potential				*/
-double	z;		/* displacement along growth axis	*/
-int	n;		/* principal quantum number		*/
-int	N;		/* number of z points per Angstrom	*/
-char	p;		/* particle (e, h, or l)		*/
-char    filename[9];    /* output filename                      */
+    if(opt.get_verbose())
+    {
+        std::cout << "alpha  = " << alpha  << " m^{-1}" << std::endl;
+        std::cout << "lambda = " << lambda << std::endl;
+        std::cout << "mass   = " << m      << " kg" << std::endl;
+    }
 
-FILE	*FE;		/* pointer to energy file 		*/
-FILE	*FV;		/* pointer to potential file 		*/
+    std::valarray<double> z(nz); // Spatial samples [m]
+    std::valarray<double> V(nz); // Potential [J]
 
-/* default values, appropriate to GaAs-GaAlAs */
-lambda=2.0;
-L=300e-10;
-m=0.067*me;
-N=1;
-p='e';
+    // Generate potential profile
+    const double dz = L / (nz -1); // Size of each spatial cell [m]
 
-while((argc>1)&&(argv[1][0]=='-'))
-{
- switch(argv[1][1])
- {
-  case 'l':
-	   lambda=atof(argv[2]);
-	   break;
-  case 'L':
-	   L=atof(argv[2])*1e-10;
-	   break;
-  case 'm':
-	   m=atof(argv[2])*me;
-	   break;
-  case 'N':
-	   N=atoi(argv[2]);
-	   break;
-  case 'p':
-           p=*argv[2];
-           switch(p)
-           {
-            case 'e': break;
-            case 'h': break;
-            case 'l': break;
-            default:  printf("Usage:  pth [-p particle (\033[1me\033[0m, h, or l)]\n");
-                      exit(0);
-           }
-           break;
-  default:
-	   printf("Usage:  pth [-a width parameter alpha (\033[1m0.1\033[0m])/A\n");
-           printf("            [-l depth parameter lambda \033[1m2.0\033[0m]\n");
-           printf("            [-L extent of z- domain (\033[1m300\033[0mA)]\n");
-	   printf("            [-m well mass (\033[1m0.067\033[0mm0)]\n");
-	   printf("            [-N points/Angstrom (\033[1m1\033[0m/A)]\n");
-           printf("            [-p particle (\033[1me\033[0m, h, or l)]\n");
-	   exit(0);
- }
- argv++;
- argv++;
- argc--;
- argc--;
-}
+    for(unsigned int iz = 0; iz < nz; ++iz)
+    {
+        // Construct array of positions centered at zero
+        z[iz] = iz*dz - L/2.0;
+        V[iz] = -gsl_pow_2(hBar*alpha/cosh(alpha*z[iz]))*lambda*(lambda-1)/(2*m);
+    }
 
-/* Write potential to file	*/
+    write_table_xy("v.r", z, V); // Write potential profile to file
 
-FV=fopen("v.r","w");
+    // Number of bound states
+    size_t nst = ceil(lambda-1); // principal quantum number
 
-z=-L/2;		/* initialise z position	*/
-do      
-{
- V=-gsl_pow_2(hBar*alpha)*lambda*(lambda-1)/(2*m*gsl_pow_2(cosh(alpha*z)));
- fprintf(FV,"%20.17le %20.17le\n",z,V);
- z+=1/((float)N)*1e-10;
-}while(z<=(L/2));
+    std::valarray<double> E(nst); // Energy of each state
 
-fclose(FV);
+    // Compute energy of state analytically
+    for(unsigned int ist = 0; ist < nst; ++ist)
+        E[ist] = -gsl_pow_2(hBar*alpha*(lambda-1-(float)ist))/(2*m);
 
-/* Write energy levels to file `Ee.r'	*/
+    E *= 1000/e; // Convert to meV
 
-sprintf(filename,"E%c.r",p);	/* define output filename	*/
-FE=fopen(filename,"w");
+    // Write energy to file
+    char filename[9];
+    sprintf(filename,"E%c.r",p);
+    write_table_x(filename, E, true);
 
-n=0;
-while((lambda-1-n)>=0)	
-{
- E=-gsl_pow_2(hBar*alpha)*gsl_pow_2(lambda-1-(float)n)/(2*m);
- 
- /* Write data to file, note n->n+1 to conform with other standards	*/
-
- fprintf(FE,"%i %20.17le\n",n+1,E/(1e-3*e));	
-
- n++;
-}
-
-fclose(FE);
-
-return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
