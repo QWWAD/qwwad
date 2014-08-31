68b3eeb6c9287f87ef560e88be66589802912f47
Alex Valavanis <valavanisalex@gmail.com>
2014-08-31 12:58:27 +0100

    Add remaining parabolic-well examples

--- examples/numerical-solutions/parabolic-well-E-10.sh
+++ examples/numerical-solutions/parabolic-well-E-10.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# Computes the eigenstates of a parabolic quantum well
+# Computes the first ten states in a parabolic well
 #
 # This script is part of the QWWAD software suite. Any use of this code
 # or its derivatives in published work must be accompanied by a citation
@@ -26,43 +26,23 @@ set -e
 # along with QWWAD.  If not, see <http://www.gnu.org/licenses/>.
 
 # Initialise files
-outfile=parabolic-well-solutions.dat
-outfile_E=parabolic-well-energies.dat
-rm -f $outfile $outfile_E
+outfile=parabolic-well-E-10.dat
+rm -f $outfile
 
-# Calculate alloy profile along z-axis
-efpqw --xmax 1
+# Calculate alloy profile along z-axis, choose artificially high potential
+efpqw --xmax 10 --nz 1000
 
-# Convert alloy concentration `x' into effective mass `m'
-efxv -m 0.067
+# Convert alloy concentration `x' into effective mass `m', specify constant
+# mass
+efxv --mass 0.067
 
 # Implement shooting technique
-efss --nst-max 3
+efss --nst-max 10
 
-wfplot --plot-wf --plot-file $outfile
-mv Ee.r $outfile_E
+mv Ee.r $outfile
 
 cat << EOF
-Results have been written to $outfile and $outfile_E, which
-contain the wavefunctions and energies, respectively, for a 
-parabolic GaAs quantum well.
-
-$outfile is in the format:
-
-  COLUMN 1 - Spatial location [angstrom]
-  COLUMN 2 - Confining potential [meV] or wavefunction amplitude [a.u.]
-
-  The file contains 4 data sets, each set being separated
-  by a blank line, representing the confining potential and the
-  wavefunctions of the first three eigenstates, which are plotted
-  at their respective energies:
-
-  SET 1 - Confining potential
-  SET 2 - State |1>
-  SET 3 - State |2>
-  SET 4 - State |3>
-
-$outfile_E is in the format:
+Results have been written to $outfile in the format:
 
   COLUMN 1 - State index
   COLUMN 2 - Energy [meV]
--- examples/numerical-solutions/poeschl-teller-hole-vary-lambda.sh
+++ examples/numerical-solutions/poeschl-teller-hole-vary-lambda.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 set -e
 
-# Define output file
-outfile_E=poeschl-teller-hole-E-lambda.dat
-outfile_V=poeschl-teller-hole-V.dat
+# Computes the eigenstates of Poeschl-Teller holes of various depths
+#
+# This script is part of the QWWAD software suite. Any use of this code
+# or its derivatives in published work must be accompanied by a citation
+# of:
+#   P. Harrison and A. Valavanis, Quantum Wells, Wires and Dots, 4th ed.
+#    Chichester, U.K.: J. Wiley, 2015, ch.2
+#
+# (c) Copyright 1996-2014
+#     Alex Valavanis <a.valavanis@leeds.ac.uk>
+#
+# QWWAD is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# QWWAD is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with QWWAD.  If not, see <http://www.gnu.org/licenses/>.
 
 # Initialise files
+outfile_E=poeschl-teller-hole-E-lambda.dat
+outfile_V=poeschl-teller-hole-V.dat
 rm -f $outfile_E $outfile_V
 
 # Define length of Poschl-Teller potential
@@ -55,3 +77,41 @@ awk '{print $1*1e10, $2*1000/1.6e-19}' v-1.5.r >> $outfile_V
 printf "\n" >> $outfile_V
 awk '{print $1*1e10, $2*1000/1.6e-19}' v-2.0.r >> $outfile_V
 printf "\n" >> $outfile_V
+
+cat << EOF
+Results have been written to $outfile_V and
+$outfile_E, containing the potential profiles and the energies
+respectively.
+
+$outfile_V in the format:
+
+  COLUMN 1 - Spatial location [angstrom]
+  COLUMN 2 - Potential [meV]
+
+  The file contains 4 data sets, each set being separated
+  by a blank line, representing different depth parameters:
+
+  SET 1 - lambda=0.75
+  SET 2 - lambda=1.00
+  SET 3 - lambda=1.50
+  SET 4 - lambda=2.00
+
+$outfile_E in the format:
+
+  COLUMN 1 - Depth parameter
+  COLUMN 2 - Energy of state |1> (analytical) [meV]
+  COLUMN 3 - Energy of state |2> (analytical) [meV]
+  COLUMN 4 - Energy of state |1> (numerical) [meV]
+  COLUMN 5 - Energy of state |2> (numerical) [meV]
+
+This script is part of the QWWAD software suite.
+
+(c) Copyright 1996-2014
+    Alex Valavanis <a.valavanis@leeds.ac.uk>
+    Paul Harrison  <p.harrison@leeds.ac.uk>
+
+Report bugs to https://bugs.launchpad.net/qwwad
+EOF
+
+# Clean up workspace
+rm -f *.r
