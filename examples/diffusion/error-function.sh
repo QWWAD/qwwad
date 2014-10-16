0d0b9b6b658da970d3529206a01b7b85b355df1a
Alex Valavanis <valavanisalex@gmail.com>
2014-10-16 23:08:25 +0100

    ex/diff: dist error-function example

---
+++ examples/diffusion/error-function.sh
@@ -0,0 +1,63 @@
+#!/bin/sh -e
+
+# Computes a diffusion profile for a single quantum well with constant diff. coeff.
+#
+# This script is part of the QWWAD software suite. Any use of this code
+# or its derivatives in published work must be accompanied by a citation
+# of:
+#   P. Harrison and A. Valavanis, Quantum Wells, Wires and Dots, 4th ed.
+#    Chichester, U.K.: J. Wiley, 2015, ch.2
+#
+# (c) Copyright 1996-2014
+#     Alex Valavanis <a.valavanis@leeds.ac.uk>
+#
+# QWWAD is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# QWWAD is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with QWWAD.  If not, see <http://www.gnu.org/licenses/>.
+
+# Initialise files
+outfile=error-function.dat
+rm -f $outfile
+
+# Create structure definition file
+cat > s.r << EOF
+200 0.1 0.0
+200 0.0 0.0
+200 0.1 0.0
+EOF
+
+# Generate alloy concentration (diffusant) profile
+find_heterostructure --dz-max 1
+
+# Solve diffusion equation
+gde --coeff 10 --time 100
+ 
+# Store diffusion profile
+awk '{print $1*1e10, $2}' X.r > $outfile
+
+cat << EOF
+Results have been written to $outfile in the format:
+
+  COLUMN 1 - Spatial location [Angstrom]
+  COLUMN 2 - Alloy concentration
+
+This script is part of the QWWAD software suite.
+
+(c) Copyright 1996-2014
+    Alex Valavanis <a.valavanis@leeds.ac.uk>
+    Paul Harrison  <p.harrison@leeds.ac.uk>
+
+Report bugs to https://bugs.launchpad.net/qwwad
+EOF
+
+# Clean up workspace
+rm -f *.r
