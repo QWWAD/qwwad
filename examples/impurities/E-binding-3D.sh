7c437469921779a868a177f35428ca6abc373648
Alex Valavanis <valavanisalex@gmail.com>
2014-11-15 14:43:49 +0000

    Dist 3D binding energy example

--- examples/impurities/E-binding-3D.sh
+++ examples/impurities/E-binding-3D.sh
@@ -1,25 +1,55 @@
-#!/bin/sh
+#! /bin/sh
+set -e
+
+# Calculate binding energy for 3D donors in QWs with varying width and depth
+#
+# This script is part of the QWWAD software suite. Any use of this code
+# or its derivatives in published work must be accompanied by a citation
+# of:
+#   P. Harrison and A. Valavanis, Quantum Wells, Wires and Dots, 4th ed.
+#    Chichester, U.K.: J. Wiley, 2015, ch.2
+#
+# (c) Copyright 1996-2014
+#     Paul Harrison  <p.harrison@shu.ac.uk>
+#     Alex Valavanis <a.valavanis@leeds.ac.uk>
+#
+# QWWAD is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# QWWAD is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with QWWAD.  If not, see <http://www.gnu.org/licenses/>.
+
 # 3D trial wave fucntion donor binding energy calculationas a function of
 # well width for different barrier heights
 # Define the barrier alloy concentration
 
 # Initialise files
 outfile_E=E-binding-3D.dat
-outfile_ED0=E-binding-3D-ED0.dat
-rm -f $outfile_E $outfile_ED0
+outfile_lambda=E-binding-3D-lambda.dat
+rm -f $outfile_E $outfile_lambda
+
+# Loop over alloy concentration
+for x in 0.1 0.2 0.3; do
 
 # Loop over well width
 for LW in 10 20 30 40 50 60 80 100 140 180 220 260 300 400 500 600 800 1000; do
 
 # Define structure
 cat > s.r << EOF
-200 0.1 0.0
+200 $x  0.0
 $LW 0.0 0.0
-200 0.1 0.0
+200 $x  0.0
 EOF
 
 # Generate alloy profile
-find_heterostructure
+find_heterostructure --dz-max 1
 
 # Generate potential profile for Ga(1-x)Al(x)As, can use defaults
 efxv --mass 0.067 
@@ -41,8 +71,61 @@ E0=`awk '{printf(" %20.17e\n",$2)}' Ee.r`
 
 # Store data to file, i.e. energy with donor (from e.r), energy
 # without donor (from Ee.r) versus well width (lw)
-echo $LW $E $E0 >> $outfile_E
+echo $LW $E $E0 | awk '{print $1, $3 - $2, 5.3}' >> $outfile_E
 
-awk '{print $1, $3 - $2, 11.7}' < $outfile_E > $outfile_ED0
-done
+# Store the Bohr radius to file
+lambda=`awk '{print $2}' l.r`
 
+echo $LW $lambda >> $outfile_lambda
+
+done # End loop over well width
+
+printf "\n" >> $outfile_E
+printf "\n" >> $outfile_lambda
+done # End loop over alloy concentration
+
+# Output the bulk binding energy
+cat >> $outfile_E << EOF
+10   5.3
+1000 5.3
+EOF
+
+# Output the bulk Bohr radius
+cat >> $outfile_lambda << EOF
+10   103
+1000 103
+EOF
+
+cat << EOF
+Results have been written to $outfile_E and $outfile_lambda.
+
+$outfile_E is in the format:
+
+  COLUMN 1 - Quantum well width [Angstrom]
+  COLUMN 2 - Binding energy [meV]
+
+$outfile_lambda is in the format:
+
+  COLUMN 1 - Quantum well width [Angstrom]
+  COLUMN 2 - Bohr radius [Angstrom]
+
+  Each file contains 4 data sets, each set being separated
+  by a blank line, representing wells with different barrier
+  compositions, and the donor in bulk GaAs for comparison.
+
+  SET 1  - 10% aluminium barriers
+  SET 2  - 20% aluminium barriers
+  SET 3  - 30% aluminium barriers 
+  SET 4  - Bulk material
+
+This script is part of the QWWAD software suite.
+
+(c) Copyright 1996-2014
+    Alex Valavanis <a.valavanis@leeds.ac.uk>
+    Paul Harrison  <p.harrison@leeds.ac.uk>
+
+Report bugs to https://bugs.launchpad.net/qwwad
+EOF
+
+# Clean up workspace
+rm -f *.r
