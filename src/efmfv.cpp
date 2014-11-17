6983d677d1ecffe0cea0ce768822429ff821972c
Alex Valavanis <valavanisalex@gmail.com>
2014-11-17 00:49:05 +0000

    efmfv: C++ify

--- src/efmfv.cpp
+++ src/efmfv.cpp
@@ -88,175 +88,146 @@ static double B_J(const double J,
 
 int main(int argc,char *argv[])
 {
-double	A;
-double	B;
-double	DeltaV;		/* change in potential due to MF	*/
-double	J;		/* magnetic ion spin			*/
-double	MF;		/* Magnetic field			*/
-double	N0alpha;	/* Magnetic parameter			*/
-double	N0beta;		/* Magnetic parameter			*/
-double	Sz;		/* effective spin			*/
-double	T;		/* sample temperature			*/
-double	T0;		/* effective temperature		*/
-double	x;		/* the alloy concentration		*/
-int	n;		/* return value of fopen		*/
-int	i;		/* index				*/
-char	p;		/* particle (e, h, or l)		*/
-char	s;		/* electron spin state, up or down, +/- */
-FILE	*Fv;		/* pointer to input potential file	*/
-FILE	*FvF;		/* pointer to v.r with field		*/
-FILE	*Fx;		/* pointer to x.r 			*/
-data11	*V0;		/* pointer to potential data		*/
-data11	*X0;		/* pointer to alloy data		*/
+    /* Define global defaults */
+    double MF      = 0.0; // Magnetic field
+    char   p       = 'e'; // Particle (e, h, or l)
+    char   s       = '+'; // Electron spin state, up or down, +/-
+    double T       = 1.8;     // Sample temperature
+    double N0alpha = 0.220*e; // Magnetic parameter
+    double N0beta  = 0.880*e; // Magnetic parameter
+    double J       = 2.5;     // magnetic ion spin
 
-/* Define global defaults */
-
-MF=0.0;
-p='e';
-s='+';
-
-T=1.8;
-N0alpha=0.220*e;
-N0beta=0.880*e;
-J=2.5;
-
-while((argc>1)&&(argv[1][0]=='-'))
-{
- switch(argv[1][1])
- {
-  case 'B':
-	   MF=atof(argv[2]);            /* read magnetic field in Tesla */
-	   break;
-  case 'p':
-           p=*argv[2];
-           switch(p)
-           {
-            case 'e': break;
-            case 'h': break;
-            case 'l': break;
-            default:  printf("Usage:  efmfv [-p particle (\033[1me\033[0m, h, or l)]\n");
-                      exit(0);
-           }
-           break;
-  case 's':
-	   s=*argv[2];
-	   break;
-  case 'T':
-	   T=atof(argv[2]);
-	   break;
-  default: 
-	   printf("Usage: efmfv [-B magnetic field (\033[1m0\033[0mTesla)][-p particle (\033[1me\033[0m, h, or l)]\n");
-	   printf("             [-s spin-state (\033[1m+\033[0m/-)][-T temperature (\033[1m1.8\033[0mK)]\n");
-	   exit(0);
- }
- argv++;
- argv++;
- argc--;
- argc--;
-}
+    while((argc>1)&&(argv[1][0]=='-'))
+    {
+        switch(argv[1][1])
+        {
+            case 'B':
+                MF=atof(argv[2]);            /* read magnetic field in Tesla */
+                break;
+            case 'p':
+                p=*argv[2];
+                switch(p)
+                {
+                    case 'e': break;
+                    case 'h': break;
+                    case 'l': break;
+                    default:  printf("Usage:  efmfv [-p particle (\033[1me\033[0m, h, or l)]\n");
+                              exit(0);
+                }
+                break;
+            case 's':
+                s=*argv[2];
+                break;
+            case 'T':
+                T=atof(argv[2]);
+                break;
+            default: 
+                printf("Usage: efmfv [-B magnetic field (\033[1m0\033[0mTesla)][-p particle (\033[1me\033[0m, h, or l)]\n");
+                printf("             [-s spin-state (\033[1m+\033[0m/-)][-T temperature (\033[1m1.8\033[0mK)]\n");
+                exit(0);
+        }
+        argv++;
+        argv++;
+        argc--;
+        argc--;
+    }
 
 
-/* Open zero field potential file */
+    /* Open zero field potential file */
+    FILE	*Fv;		/* pointer to input potential file	*/
+    if((Fv=fopen("v0.r","r"))==0)
+    {
+        if((system("cp v.r v0.r"))==0) Fv=fopen("v0.r","r");
+        else 
+        {printf("Error: Cannot open file 'v0.r' or find file 'v.r'!\n");exit(0);}
+    }
 
-if((Fv=fopen("v0.r","r"))==0)
- {
-  if((system("cp v.r v0.r"))==0) Fv=fopen("v0.r","r");
-  else 
-   {printf("Error: Cannot open file 'v0.r' or find file 'v.r'!\n");exit(0);}
- }
+    /* Open structure file `x.r'	*/
+    FILE	*Fx;		/* pointer to x.r 			*/
+    if((Fx=fopen("x.r","r"))==NULL)
+    {printf("Error: Cannot open file 'x.r'!\n");exit(0);}
 
-/* Open structure file `x.r'	*/
+    /* Count number of lines in potential file */
+    int n=0;
+    while(fscanf(Fv,"%*e %*e")!=EOF)
+        n++;
+    rewind(Fv);
 
-if((Fx=fopen("x.r","r"))==NULL)
- {printf("Error: Cannot open file 'x.r'!\n");exit(0);}
+    /* Allocate memory for potential and alloy concentration structures */
 
-/* Count number of lines in potential file */
+    data11 *V0=(data11 *)calloc(n,sizeof(data11));
+    if(V0==0){fprintf(stderr,"Cannot allocate memory!\n");exit(0);}
 
-n=0;
-while(fscanf(Fv,"%*e %*e")!=EOF)
- n++;
-rewind(Fv);
+    data11 *X0=(data11 *)calloc(n,sizeof(data11));
+    if(X0==0){fprintf(stderr,"Cannot allocate memory!\n");exit(0);}
 
-/* Allocate memory for potential and alloy concentration structures */
+    /* Read data into structure */
 
-V0=(data11 *)calloc(n,sizeof(data11));
- if(V0==0){fprintf(stderr,"Cannot allocate memory!\n");exit(0);}
+    int i=0;
+    while(fscanf(Fv,"%le %le",&((V0+i)->a),&((V0+i)->b))!=EOF)
+    {
+        /* Note line below assumes magnetic ion in `x' column of `s.r'	*/
 
-X0=(data11 *)calloc(n,sizeof(data11));
- if(X0==0){fprintf(stderr,"Cannot allocate memory!\n");exit(0);}
+        int n_read = fscanf(Fx,"%le %le %*e",&((X0+i)->a),&((X0+i)->b));
+        if(n_read == 3)
+            i++;
+    }
 
-/* Read data into structure */
+    fclose(Fv);
+    fclose(Fx);
 
-i=0;
-while(fscanf(Fv,"%le %le",&((V0+i)->a),&((V0+i)->b))!=EOF)
-{
- /* Note line below assumes magnetic ion in `x' column of `s.r'	*/
+    /* Add field in the form +/- 3A, +/- 3B or +/- B depending on spin */
+    for(int i=0;i<n;i++)
+    {
+        const double x=(X0+i)->b;
+        const double T0 = Teff(x); // Effective temperature
+        const double Sz = Seff(N0alpha,N0beta,x);
 
- int n_read = fscanf(Fx,"%le %le %*e",&((X0+i)->a),&((X0+i)->b));
- if(n_read == 3)
-   i++;
-}
+        const double A=x*N0alpha*Sz*B_J(J,MF,T,T0)/6;
+        const double B=x*N0beta*Sz*B_J(J,MF,T,T0)/6;
 
-fclose(Fv);
-fclose(Fx);
+        /* calculate change in potential DeltaV due to field	*/
+        double DeltaV=3*A; /* Default value */
+        switch(p)
+        {
+            case 'e': 
+                switch(s)
+                {
+                    case '+':DeltaV=3*A;break;
+                    case '-':DeltaV=-3*A;break;
+                }
+                break;
+            case 'h':
+                switch(s)
+                {
+                    case '+':DeltaV=3*B;break;
+                    case '-':DeltaV=-3*B;break;
+                }
+                break;
+            case 'l': 
+                switch(s)
+                {
+                    case '+':DeltaV=B;break;
+                    case '-':DeltaV=B;break;
+                }
+                break;
+        }
 
+        ((V0+i)->b)+=DeltaV;
+    }
 
+    /* Write data to file 'v.r' */
+    FILE	*FvF;		/* pointer to v.r with field		*/
+    if((FvF=fopen("v.r","w"))==NULL)
+        printf("Error: Cannot open file 'v.r' to output data!\n");
 
+    for(i=0;i<n;i++)
+        fprintf(FvF,"%20.17le %20.17le\n",(V0+i)->a,(V0+i)->b);
 
+    /* Close files */
 
-/* Add field in the form +/- 3A, +/- 3B or +/- B depending on spin */
+    fclose(FvF);
 
-for(i=0;i<n;i++)
-{
- x=(X0+i)->b;
- T0=Teff(x);
- Sz=Seff(N0alpha,N0beta,x);
-
- A=x*N0alpha*Sz*B_J(J,MF,T,T0)/6;
- B=x*N0beta*Sz*B_J(J,MF,T,T0)/6;
-
-/* calculate change in potential DeltaV due to field	*/
-DeltaV=3*A; /* Default value */
-switch(p)
-{
- case 'e': 
-	  switch(s)
-	  {
-	   case '+':DeltaV=3*A;break;
-	   case '-':DeltaV=-3*A;break;
-	  }
-	  break;
- case 'h':
-	  switch(s)
-	  {
-	   case '+':DeltaV=3*B;break;
-	   case '-':DeltaV=-3*B;break;
-	  }
-	  break;
- case 'l': 
-	  switch(s)
-	  {
-	   case '+':DeltaV=B;break;
-	   case '-':DeltaV=B;break;
-	  }
-	  break;
-}
-
- ((V0+i)->b)+=DeltaV;
-}
-
-/* Write data to file 'v.r' */
-
-if((FvF=fopen("v.r","w"))==NULL)
- printf("Error: Cannot open file 'v.r' to output data!\n");
-
-for(i=0;i<n;i++)
- fprintf(FvF,"%20.17le %20.17le\n",(V0+i)->a,(V0+i)->b);
-
-/* Close files */
-
-fclose(FvF);
-
-return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 // vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99 :
