32af10155df230de2d7699c4fe0ff25a154b07b1
Alex Valavanis <valavanisalex@gmail.com>
2014-12-10 16:37:27 +0000

    i0: Pre-calculate a few values to speed things up

--- src/i0.cpp
+++ src/i0.cpp
@@ -32,6 +32,10 @@
 #include <cstdlib>
 #include <cstring>
 #include <cmath>
+#include <iostream>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_integration.h>
+#include <gsl/gsl_min.h>
 #include "struct.h"
 #include "qclsim-constants.h"
 #include "qclsim-fileio.h"
@@ -40,14 +44,20 @@
 using namespace Leeds;
 using namespace constants;
 
-double Energy(const std::valarray<double> &wf,
-              const std::valarray<double> &V,
-              const std::valarray<double> &z,
-              double	epsilon,
-              double	m,
-              double	lambda,
-              double	r_i,
-              int	S);
+struct EnergyParams
+{
+    std::valarray<double> wf;
+    std::valarray<double> V;
+    std::valarray<double> z;
+    double                epsilon;
+    double                m;
+    double                r_i;
+    int	                  S;
+};
+
+double Energy(double lambda,
+              void   *params);
+
 double Psi(const double psi,
            const double lambda,
            const double x,
@@ -57,27 +67,14 @@ double Psi(const double psi,
 
 int main(int argc,char *argv[])
 {
-double	epsilon;	/* permitivity of material		*/
-double	f,fdash;	/* function (dE/dlambda) and derivative 
-			   to be solved				*/
-double	lambda;		/* Bohr radius (variational)		*/
-double	lambda_step;	/* Bohr radius increment		*/
-double	lambda_0;	/* Bohr radius of bulk impurity		*/
-double	m;		/* mass of particle			*/
-double	y1,y2,y3;	/* E(lambda-), E(lambda), E(lambda+)	*/
-int	state;		/* principal quantum number		*/
-int	S;		/* impurity level, i.e. `1s', `2px' etc	*/
-char	p;		/* particle				*/
 char	State[9];	/* string containing impurity level	*/
-FILE   *fe;		/* file pointer for energies		*/
-FILE   *fl;		/* file pointer for lambda_0		*/
 
 /* default values */
-epsilon=13.18*eps0;
-m=0.067*me;
-p='e';
-state=1;
-S=1;
+double epsilon = 13.18*eps0; // Permittivity [F/m]
+double m       = 0.067*me;   // Mass of particle [kg]
+char   p       = 'e';        // Particle ID
+int    state   = 1;          // Principal quantum number
+int    S       = 1;          // Impurity level `1s', `2px', etc
 
 while((argc>1)&&(argv[1][0]=='-'))
 {
@@ -143,12 +140,12 @@ while((argc>1)&&(argv[1][0]=='-'))
     std::valarray<double> wf;    // Wave function samples at each point [m^{-1/2}]
     read_table_xy(filename, z_tmp, wf);
 
-  lambda_0=4*pi*epsilon*(hBar/e)*(hBar/e)/m;/* Bohr	theory (1s)	*/
+  const double lambda_0=4*pi*epsilon*(hBar/e)*(hBar/e)/m;/* Bohr	theory (1s)	*/
 
   /* Open files for output of data */
 
-  fe=fopen("e.r","w");           /* E versus r_i	*/
-  fl=fopen("l.r","w");           /* lambda versus r_i	*/
+  FILE *fe=fopen("e.r","w");           /* E versus r_i	*/
+  FILE *fl=fopen("l.r","w");           /* lambda versus r_i	*/
 
   // Read list of donor (or acceptor) positions
   std::valarray<double> r_d; // [m]
@@ -157,33 +154,45 @@ while((argc>1)&&(argv[1][0]=='-'))
   // Perform variational calculation for each donor/acceptor position
   for(unsigned int i_d = 0; i_d < r_d.size(); ++i_d)
   {
-   lambda=lambda_0;				/* initial lambda guess	*/
-   if((S==2)||(S==3))lambda*=2;			/* for 2s, 2px NOT 2pz	*/
-
-   lambda_step=lambda/100;
+   double lambda=lambda_0;	// initial lambda guess
+   if((S==2)||(S==3))lambda*=2;	// for 2s, 2px NOT 2pz
 
    /* Newton-Raphson iteration for solution of lambda, this occurs when
       dE/dlambda=0, hence the function f is dE/dlambda and f'=d2E/dlambda^2
     								*/
+   EnergyParams params = {wf, V, z, epsilon, m, r_d[i_d], S};
+
+   // Set up the numerical solver using GSL
+   gsl_function f;
+   f.function = &Energy;
+   f.params   = &params;
+
+   gsl_min_fminimizer *s = gsl_min_fminimizer_alloc(gsl_min_fminimizer_brent);
+   gsl_min_fminimizer_set(s, &f, lambda, lambda/5, lambda*5);
+
+   size_t max_iter = 100; // Maximum number of iterations before giving up
+   int status = 0;        // Error flag for GSL
+   unsigned int iter=0;   // The number of iterations attempted so far
+
+   double E = 1000*e; // Minimum energy of carrier [J]
+
+   // Variational calculation (search over lambda)
    do
    {
-    y1=Energy(wf,V,z,epsilon,m,lambda-lambda_step,r_d[i_d],S);
-    y2=Energy(wf,V,z,epsilon,m,lambda,r_d[i_d],S);
-    y3=Energy(wf,V,z,epsilon,m,lambda+lambda_step,r_d[i_d],S);
+       ++iter;
+       status  = gsl_min_fminimizer_iterate(s);
+       const double lambda_lo = gsl_min_fminimizer_x_lower(s);
+       const double lambda_hi = gsl_min_fminimizer_x_upper(s);
+       lambda = gsl_min_fminimizer_x_minimum(s);
+       E      = gsl_min_fminimizer_f_minimum(s);
+       status = gsl_min_test_interval(lambda_lo, lambda_hi, 0.1e-10, 0.0);
+       printf("r_d %le lambda %le energy %le meV\n", r_d[i_d], lambda, E/(1e-3*e));
+   }while((status == GSL_CONTINUE) && (iter < max_iter));
 
-    f=(y3-y1)/(2*lambda_step);
-    fdash=(y3-2*y2+y1)/(lambda_step*lambda_step);
-
-    printf("r_i %4.2f A lambda %4.2f A energy %4.3f meV\n",
-            r_d[i_d]/1e-10,lambda/1e-10,y2/(1e-3*e));
-
-    lambda-=f/fdash;
-
-   }while(fabs(f/fdash)>1e-10);	
-
+   gsl_min_fminimizer_free(s);
    /* Output total energy (E) of impurity/heterostructure system 
       and Bohr radii (lambda), in meV and Angstrom respectively */
-   fprintf(fe,"%le %le\n",r_d[i_d]/1e-10,y2/(1e-3*e));
+   fprintf(fe,"%le %le\n",r_d[i_d]/1e-10,E/(1e-3*e));
    fprintf(fl,"%le %le\n",r_d[i_d]/1e-10,lambda/1e-10);
   }/* end while r_i */
 
@@ -195,109 +204,121 @@ while((argc>1)&&(argv[1][0]=='-'))
 
 /* This function calculates the expectation value (the energy) of the
    Hamiltonian operator	*/
-double Energy(const std::valarray<double> &wf,
-              const std::valarray<double> &V,
-              const std::valarray<double> &z,
-              double	epsilon,
-              double	m,
-              double	lambda,
-              double	r_i,
-              int	S)
+double Energy(double  lambda,
+              void   *params)
 {
- const double dz  = z[1] - z[0]; // z- (growth) direction step length [m]
- const size_t nz  = V.size();    // Number of spatial samples in z direction
- const double dxy = lambda/10;   // Step size for in-plane integration [m]
- const size_t nxy = 31;          // Number of samples to use in integration over x and y
+    EnergyParams *p = reinterpret_cast<EnergyParams *>(params);
+    const double dz  = p->z[1] - p->z[0]; // z- (growth) direction step length [m]
+    const size_t nz  = p->z.size();       // Number of spatial samples in z direction
+    const double dxy = lambda/10;         // Step size for in-plane integration [m]
+    const size_t nxy = 31;                // Number of samples to use in integration over x and y
 
- // Integrands wrt z for calculating wavefunction overlap
- // and Hamiltonian
- std::valarray<double> PD_integrand_z(nz);
- std::valarray<double> H_integrand_z(nz);
+    // Integrands wrt z for calculating wavefunction overlap
+    // and Hamiltonian
+    std::valarray<double> PD_integrand_z(nz);
+    std::valarray<double> H_integrand_z(nz);
 
- // Compute integrand over the z-axis, skipping both end-points since we
- // need the 2nd derivatives
- for(unsigned int iz=1;iz < nz-1;iz++)
- {
-     // Integrands wrt (x,z) for calculating wavefunction overlap
-     // and Hamiltonian
-     std::valarray<double> PD_integrand_xz(nxy);
-     std::valarray<double> H_integrand_xz(nxy);
+    // Pre-calculate a couple of params to speed things up
+    const double hBar_sq_by_2m  = hBar*hBar/(2.0*p->m);
+    const double e_sq_by_4pieps = e*e/(4.0*pi*p->epsilon);
 
-     // integrate over the plane, skipping singularity at x=0
-     // Note that we correct for this later
-     for(unsigned int ix=1; ix<nxy; ++ix)	
-     {
-         const double x = ix*dxy;
+    // Compute integrand over the z-axis, skipping both end-points since we
+    // need the 2nd derivatives
+    for(unsigned int iz=1;iz < nz-1;iz++)
+    {
+        // Integrands wrt (x,z) for calculating wavefunction overlap
+        // and Hamiltonian
+        std::valarray<double> PD_integrand_xz(nxy);
+        std::valarray<double> H_integrand_xz(nxy);
 
-         // Integrands wrt (x,y,z) for calculating wavefunction overlap
-         // and Hamiltonian
-         std::valarray<double> PD_integrand_xyz(nxy);
-         std::valarray<double> H_integrand_xyz(nxy);
+        const double z_dash = p->z[iz] - p->r_i; // Separation from donor in z-direction [m]
 
-         for(unsigned int iy=1; iy<nxy; ++iy)
-         {
-             const double y = iy*dxy;
+        // integrate over the plane, skipping singularity at x=0
+        // Note that we correct for this later
+        for(unsigned int ix=1; ix<nxy; ++ix)	
+        {
+            const double x = ix*dxy;
 
-             // Wavefunction at this point
-             const double Psixyz = Psi(wf[iz],lambda,x,y,z[iz]-r_i,S);
+            // Integrands wrt (x,y,z) for calculating wavefunction overlap
+            // and Hamiltonian
+            std::valarray<double> PD_integrand_xyz(nxy);
+            std::valarray<double> H_integrand_xyz(nxy);
 
-             // Calculate the second derivatives along x, y and z
-             const double d2Pdx2=(Psi(wf[iz],lambda,x+dxy,y,z[iz]-r_i,S)-
-                     2*Psixyz+
-                     Psi(wf[iz],lambda,x-dxy,y,z[iz]-r_i,S))/(dxy*dxy);
+            // Wavefunction at (x, y - dy, z_dash)
+            double Psixyz_last_y = Psi(p->wf[iz], lambda, x, 0, z_dash, p->S);
 
-             const double d2Pdy2=(Psi(wf[iz],lambda,x,y+dxy,z[iz]-r_i,S)-
-                     2*Psixyz+
-                     Psi(wf[iz],lambda,x,y-dxy,z[iz]-r_i,S))/(dxy*dxy);
+            // Wavefunction at (x,y,z_dash)
+            double Psixyz = Psi(p->wf[iz],lambda, x, dxy, z_dash, p->S);
 
-             const double d2Pdz2=(Psi(wf[iz+1],lambda,x,y,z[iz+1]-r_i,S)-
-                     2*Psixyz+
-                     Psi(wf[iz-1],lambda,x,y,z[iz-1]-r_i,S))/(dz*dz);
+            const double rsq_xz = x*x + z_dash*z_dash;
 
-             // Distance from impurity for Coloumb term [m]
-             const double r=gsl_hypot3(x, y, (z[iz]-r_i));
+            for(unsigned int iy=1; iy<nxy; ++iy)
+            {
+                const double y = iy*dxy;
 
-             // The Laplacian of Psi
-             const double laplace_Psi = d2Pdx2 + d2Pdy2 + d2Pdz2;
+                // Wavefunction at (x, y+dy, z_dash)
+                const double Psixyz_next_y = Psi(p->wf[iz],lambda,x,y+dxy, z_dash, p->S);
 
-             // The integrand for the Hamiltonian expectation value
-             // QWWAD 3, Eq. 5.142
-             H_integrand_xyz[iy] = Psixyz*(-hBar*hBar/(2*m)*laplace_Psi
-                     +(V[iz]-e*e/(4*pi*epsilon*r))*Psixyz);
+                // Calculate the second derivatives along x, y and z
+                const double d2Pdx2=(Psi(p->wf[iz],lambda,x+dxy,y, z_dash, p->S)-
+                        2*Psixyz+
+                        Psi(p->wf[iz],lambda,x-dxy,y, z_dash, p->S))/(dxy*dxy);
 
-             PD_integrand_xyz[iy] = Psixyz*Psixyz;
-         }
-         // Approximate the singularities with values at neighbouring point
-         // Note that this preserves the symmetry of the functions around 0.
-         PD_integrand_xyz[0] = PD_integrand_xyz[1];
-         H_integrand_xyz[0]  = H_integrand_xyz[1];
+                const double d2Pdy2=(Psixyz_next_y - 2*Psixyz + Psixyz_last_y)/(dxy*dxy);
 
-      // Perform integration over y, noting that a factor of 2 is included
-      // to account for even symmetry
-      H_integrand_xz[ix]  = 2*simps(H_integrand_xyz, dxy);
-      PD_integrand_xz[ix] = 2*simps(PD_integrand_xyz, dxy);
-  }
+                const double d2Pdz2=(Psi(p->wf[iz+1],lambda,x,y,p->z[iz+1]-p->r_i,p->S)-
+                        2*Psixyz+
+                        Psi(p->wf[iz-1],lambda,x,y,p->z[iz-1]-p->r_i,p->S))/(dz*dz);
 
-  // Approximate the singularities with values at neighbouring point
-  // Note that this preserves the symmetry of the functions around 0.
-  PD_integrand_xz[0] = PD_integrand_xz[1];
-  H_integrand_xz[0]  = H_integrand_xz[1];
+                // Distance from impurity for Coloumb term [m]
+                const double r = sqrt(y*y + rsq_xz);
 
-  // Perform integration over x, noting that a factor of 2 is included
-  // to account for even symmetry
-  PD_integrand_z[iz] = 2*simps(PD_integrand_xz, dxy);
-  H_integrand_z[iz]  = 2*simps(H_integrand_xz, dxy);
- }
+                // The Laplacian of Psi
+                const double laplace_Psi = d2Pdx2 + d2Pdy2 + d2Pdz2;
 
- // Note that endpoints of the integral can keep their default value of zero, since
- // psi decays to zero at infinity
+                // The integrand for the Hamiltonian expectation value
+                // QWWAD 3, Eq. 5.142
+                H_integrand_xyz[iy] = Psixyz*(-hBar_sq_by_2m*laplace_Psi
+                        +(p->V[iz]-e_sq_by_4pieps/r)*Psixyz);
 
- // Compute the final value of the energy using Eq. 5.141, QWWAD3
- const double H_exp = integral(H_integrand_z, dz);
- const double norm  = integral(PD_integrand_z, dz);
- const double E = H_exp/norm;
+                PD_integrand_xyz[iy] = Psixyz*Psixyz;
 
- return E;
+                // Reuse values for next iteration
+                Psixyz_last_y = Psixyz;
+                Psixyz        = Psixyz_next_y;
+            }
+
+            // Approximate the singularities with values at neighbouring point
+            // Note that this preserves the symmetry of the functions around 0.
+            PD_integrand_xyz[0] = PD_integrand_xyz[1];
+            H_integrand_xyz[0]  = H_integrand_xyz[1];
+
+            // Perform integration over y, noting that a factor of 2 is included
+            // to account for even symmetry
+            H_integrand_xz[ix]  = 2*simps(H_integrand_xyz, dxy);
+            PD_integrand_xz[ix] = 2*simps(PD_integrand_xyz, dxy);
+        }
+
+        // Approximate the singularities with values at neighbouring point
+        // Note that this preserves the symmetry of the functions around 0.
+        PD_integrand_xz[0] = PD_integrand_xz[1];
+        H_integrand_xz[0]  = H_integrand_xz[1];
+
+        // Perform integration over x, noting that a factor of 2 is included
+        // to account for even symmetry
+        PD_integrand_z[iz] = 2*simps(PD_integrand_xz, dxy);
+        H_integrand_z[iz]  = 2*simps(H_integrand_xz, dxy);
+    }
+
+    // Note that endpoints of the integral can keep their default value of zero, since
+    // psi decays to zero at infinity
+
+    // Compute the final value of the energy using Eq. 5.141, QWWAD3
+    const double H_exp = integral(H_integrand_z, dz);
+    const double norm  = integral(PD_integrand_z, dz);
+    const double E = H_exp/norm;
+
+    return E;
 }
 
 /**
