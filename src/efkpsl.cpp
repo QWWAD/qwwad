9586fbdbaae32227aba62dc29a920fe8970e3d59
Alex Valavanis <valavanisalex@gmail.com>
2015-06-14 11:25:50 +0100

    efkpsl: Pushed to API (wf still not working)

--- src/efkpsl.cpp
+++ src/efkpsl.cpp
@@ -1,65 +1,19 @@
 /**
  * \file    efkpsl.cpp
  * \brief   Calculate the energy levels for a Kronig-Penney superlattice
- * \author  Paul Harrison  <p.harrison@shu.ac.uk>
  * \author  Alex Valavanis <a.valavanis@leeds.ac.uk>
- *
- * \details This program calculates the energy levels for a user 
- *          supplied wave vector along the growth (z-) axis of an 
- *          infinite Kronig-Penney superlattice.
- *          The code is based around:
-
- Different masses in well and barrier
-
- -hbar^2 d^2 Y + V Y = E Y,   0<z<a, V=0, a<z<b, V=V
- ------  ----                 
- 2m*   dz^2                
-
- with the additional constraint of the boundary conditions
-
- Y and 1 dY , continuous
- - --
- m dz
-
- this represents the Hamiltonian P 1 P + V
- -
- m
-
- The system is solved by expressing the
- standard condition as a function f(x)=0 and
- implementing a Newton-Raphson iterative method
- where the independent variable x is the energy.
- The first estimate of the energy is found by 
- following the function along the x axis until it
- changes sign then using a midpoint rule.
  */
 
 #include <iostream>
 #include <cstdlib>
-#include <gsl/gsl_errno.h>
-#include <gsl/gsl_math.h>
-#include <gsl/gsl_roots.h>
 #include <valarray>
 #include "qwwad/options.h"
 #include "qwwad/constants.h"
 #include "qwwad/file-io.h"
+#include "qwwad/schroedinger-solver-kronig-penney.h"
 
 using namespace QWWAD;
 using namespace constants;
-
-/// Parameters needed for computing superlattice eigenstates
-struct kpsl_params
-{
-    double a;           ///< Well width [m]
-    double b;           ///< Barrier width [m]
-    double m_w;         ///< Well mass [kg]
-    double m_b;         ///< Barrier mass [kg]
-    double V;           ///< Barrier potential [J]
-    double k;           ///< Wave vector [1/m]
-};
-
-static double f(double  energy,
-                void   *params);
 
 /**
  * Configure command-line options for the program
@@ -75,10 +29,13 @@ Options configure_options(int argc, char* argv[])
     opt.add_option<double>("barrier-width,b", 100,   "Width of barrier [angstrom].");
     opt.add_option<double>("well-mass,m",     0.067, "Effective mass in well (relative to that of a free electron)");
     opt.add_option<double>("barrier-mass,n",  0.067, "Effective mass in barrier (relative to that of a free electron)");
+    opt.add_option<bool>  ("output-potential",       "Output the potential profile for the system to v.r");
     opt.add_option<char>  ("particle,p",       'e',  "ID of particle to be used: 'e', 'h' or 'l', for "
                                                      "electrons, heavy holes or light holes respectively.");
+    opt.add_option<size_t>("nz,N",             1000, "Number of spatial points for output file.");
     opt.add_option<size_t>("nst,s",              1,  "Number of states to find");
     opt.add_option<double>("potential",        100,  "Barrier potential [meV]");
+    opt.add_option<double>("E-cutoff",               "Cut-off energy for solutions [meV]");
     
     opt.add_prog_specific_options_and_parse(argc, argv, summary);
 
@@ -97,112 +54,30 @@ int main(int argc,char *argv[])
     const auto V    = opt.get_option<double>("potential") * e / 1000;
     const auto nst  = opt.get_option<size_t>("nst");
     const auto k    = opt.get_option<double>("wave-vector") * pi/(a+b);   // [1/m]
+    const auto N     = opt.get_option<size_t>("nz");               // number of spatial steps
 
-    kpsl_params params = {a, b, m_w, m_b, V, k};
-    gsl_function F;
-    F.function = &f;
-    F.params   = &params;
-    auto solver = gsl_root_fsolver_alloc(gsl_root_fsolver_brent);
+    SchroedingerSolverKronigPenney se(a, b, V, m_w, m_b, k, N, 4, nst);
 
-    const double dx=1e-3*e; // arbitrarily small energy increment---0.1meV
+    // Set cut-off energy if desired
+    if(opt.vm.count("E-cutoff") > 0)
+        se.set_E_cutoff(opt.get_option<double>("E-cutoff") * e/1000);
 
-    double Elo=dx;    // first energy estimate
-    std::valarray<double> E(nst);
+    // Dump to file
+    char energy_filename[9];
+    sprintf(energy_filename,"E%c.r",p);
 
-    for(unsigned int ist=0; ist<nst; ++ist)
-    {
-        // Shift the lower estimate up past the last state we found
-        if(ist > 0) Elo = E[ist-1] + dx;
+    char wf_prefix[9];
+    sprintf(wf_prefix,"wf_%c",p);
+    Eigenstate::write_to_file(energy_filename,
+                              wf_prefix,
+                              ".r",
+                              se.get_solutions(true),
+                              true);
 
-        // Value for y=f(x) at bottom of search range
-        const double y1 = GSL_FN_EVAL(&F,Elo);
-
-        // Find the range in which the solution lies by incrementing the
-        // upper limit of the search range until the function changes sign.
-        // Since this coarse search can require many iterations, we keep the
-        // lower limit fixed to minimise the amount of computation at each step.
-        // The Brent algorithm is extremely fast, so it really doesn't matter that
-        // the range we find here is large.
-        //
-        // Note the end stop to prevent infinite loop in absence of solution
-        //
-        // TODO: Make the cut-off configurable
-        double y2 = y1;
-        double Ehi = Elo;
-        do
-        {
-            Ehi+=dx;
-            y2=GSL_FN_EVAL(&F, Ehi);
-        }while((y1*y2>0)&&(Ehi<100*V));
-
-        E[ist] = (Elo + Ehi)/2;
-        gsl_root_fsolver_set(solver, &F, Elo, Ehi);
-        int status = 0;
-
-        // Improve the estimate of the solution using the Brent algorithm
-        // until we hit a desired level of precision
-        do
-        {
-            status = gsl_root_fsolver_iterate(solver);
-            E[ist] = gsl_root_fsolver_root(solver);
-            Elo = gsl_root_fsolver_x_lower(solver);
-            Ehi = gsl_root_fsolver_x_upper(solver);
-            status = gsl_root_test_interval(Elo, Ehi, 1e-9*e, 0);
-        }while(status == GSL_CONTINUE);
-    }
-
-    // Output energies to file
-    char filename[9];
-    sprintf(filename,"E%c.r",p);
-    E *= 1e3/e; // Rescale to meV
-    write_table(filename, E, true);
+    // Write potential profile to file if wanted
+    if(opt.get_option<bool>("output-potential"))
+        write_table("v.r", se.get_z(), se.get_V());
 
     return EXIT_SUCCESS;
 }
-
-/**
- * \brief This function is the standard fw result = 0
- *
- * \param[in] energy Local energy
- * \param[in] params Superlattice parameters (cast to kpsl_params)
- */
-static double f(double  energy,
-                void   *params)
-{
-    const kpsl_params *p = reinterpret_cast<kpsl_params *>(params);
-    const double a   = p->a;
-    const double b   = p->b;
-    const double m_b = p->m_b;
-    const double m_w = p->m_w;
-    const double V   = p->V;
-    const double k   = p->k;
-
-    double F; // value of function
-
-    // Wave vector inside well [QWWAD3, 2.155]
-    const double k_w=sqrt(2*m_w/hBar*energy/hBar);
-
-    if(energy<V)
-    {
-        // If we're below the barrier, then the wavefunction decays with
-        // [QWWAD3, 2.167]
-        const double K=sqrt(2*m_b/hBar*(V-energy)/hBar);
-
-        // Matching equation [QWWAD3, 2.169]
-        F=cos(k_w*a)*cosh(K*b)-sin(k_w*a)*sinh(K*b)*
-            (gsl_pow_2(m_b*k_w)-gsl_pow_2(m_w*K))/(2*m_w*m_b*k_w*K)-cos(k*(a+b));
-    }
-    else
-    {
-        // If we're above the barrier, then the wavefunction propagates
-        // with [QWWAD3, 2.155]
-        const double k_b=sqrt(2*m_b/hBar*(energy-V)/hBar);
-
-        // Matching equation [QWWAD3, 2.166]
-        F=cos(k_w*a)*cos(k_b*b)-sin(k_w*a)*sin(k_b*b)*
-            (gsl_pow_2(m_b*k_w)+gsl_pow_2(m_w*k_b))/(2*m_w*m_b*k_w*k_b)-cos(k*(a+b));
-    }
-
-    return F;
-}     
 // vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99 :
---
+++ src/qwwad/schroedinger-solver-kronig-penney.cpp
@@ -0,0 +1,270 @@
+/**
+ *  \file   schroedinger-solver-kronig-penney.cpp
+ *  \author Alex Valavanis <a.valavanis@leeds.ac.uk>
+ *  \brief  Implementatation of Schrodinger solver functions for Kronig-Penney superlattice
+ */
+
+#include "schroedinger-solver-kronig-penney.h"
+
+#include <cmath>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_roots.h>
+#include "constants.h"
+#include "maths-helpers.h"
+#include <complex>
+#include <armadillo>
+
+namespace QWWAD
+{
+using namespace constants;
+SchroedingerSolverKronigPenney::SchroedingerSolverKronigPenney(const double l_w,
+                                                               const double l_b,
+                                                               const double V0,
+                                                               const double m_w,
+                                                               const double m_b,
+                                                               const double k,
+                                                               const size_t nz,
+                                                               const size_t nper,
+                                                               const unsigned int nst_max) :
+    SchroedingerSolver(std::valarray<double>(nz*nper),
+                       std::valarray<double>(nz*nper),
+                       nst_max),
+    _l_w(l_w),
+    _l_b(l_b),
+    _V0(V0),
+    _m_w(m_w),
+    _m_b(m_b),
+    _k(k)
+{
+    const auto L  = l_b + l_w;
+    const auto dz = L*nper/(nz*nper+1);
+
+    // Generate potential profile (well, then barrier)
+    for(unsigned int iper=0; iper < nper; ++iper)
+    {
+        for (unsigned int iz=0; iz<nz; iz++)
+        {
+            _z[iz + iper*nz] = iz*dz + iper*L;
+
+            // Fill in barrier potential
+            if(_z[iz] > l_w)
+                _V[iz + iper*nz] = _V0;
+        }
+    }
+}
+
+/**
+ * \brief Finds the right-hand side of the matching equation for a finite well
+ *
+ * \returns The right-hand side of the matching equation
+ */
+double SchroedingerSolverKronigPenney::get_rhs() const
+{
+    const auto L   = _l_w + _l_b; // Total length of period (well + barrier) [m]
+    const auto rhs = cos(_k * L); // Right-hand side of matching equation
+    return rhs;
+}
+
+/**
+ * \brief Finds the left-hand side of the matching equation
+ *
+ * \param[in] E Energy [J]
+ *
+ * \returns the left-hand side of the matching equation
+ */
+double SchroedingerSolverKronigPenney::get_lhs(const double E) const
+{
+    // Wave vector inside well [QWWAD3, 2.155]
+    const auto k_w = sqrt(2*_m_w*E)/hBar;
+
+    double lhs = 0.0;
+
+    if(E < _V0)
+    {
+        // If we're below the barrier, then the wavefunction decays
+        // [QWWAD3, 2.167]
+        const auto kappa = sqrt(2.0*_m_b*(_V0-E))/hBar;
+
+        // Left-hand side of matching equation [QWWAD3, 2.169]
+        lhs = cos(k_w*_l_w) * cosh(kappa*_l_b)
+            - sin(k_w*_l_w) * sinh(kappa*_l_b) * (_m_b*_m_b*k_w*k_w - _m_w*_m_w*kappa*kappa)/(2.0*_m_w*_m_b*k_w*kappa);
+    }
+    else
+    {
+        // If we're above the barrier, then the wavefunction propagates
+        // with [QWWAD3, 2.155]
+        const auto k_b=sqrt(2.0*_m_b*(E-_V0))/hBar;
+
+        // Left-hand side of matching equation [QWWAD3, 2.166]
+        lhs = cos(k_w*_l_w) * cos(k_b*_l_b)
+            - sin(k_w*_l_w) * sin(k_b*_l_b) * (_m_b*_m_b*k_w*k_w + _m_w*_m_w*k_b*k_b)/(2.0*_m_w*_m_b*k_w*k_b);
+    }
+
+    return lhs;
+}
+
+arma::cx_mat SchroedingerSolverKronigPenney::get_matching_matrix(const double E) const
+{
+    const auto I = std::complex<double>(0,1);
+    const auto L   = _l_w + _l_b; // Period length
+
+    // Find local wave vector at this energy
+    const auto k_w = sqrt(2.0*_m_w*E)/hBar; // wave vector in the well
+    const std::complex<double> k_b = sqrt(2.0*_m_b*std::complex<double>(E-_V0,0))/hBar; // Wave vector in barrier
+
+    arma::cx_mat M(4,4);
+    M(0,0) = +exp(+I*_k *L);
+    M(0,1) = +exp(+I*_k *L);
+    M(0,2) = -exp(+I*k_b*L);
+    M(0,3) = -exp(-I*k_b*L);
+    M(1,0) = +k_w/_m_w * exp(+I*_k *L);
+    M(1,1) = -k_w/_m_w * exp(+I*_k *L);
+    M(1,2) = -k_b/_m_b * exp(+I*k_b*L);
+    M(1,3) = +k_b/_m_b * exp(-I*k_b*L);
+    M(2,0) = +exp(+I*k_w*_l_w);
+    M(2,1) = +exp(-I*k_w*_l_w);
+    M(2,2) = -exp(+I*k_b*_l_w);
+    M(2,3) = -exp(-I*k_b*_l_w);
+    M(3,0) = +k_w/_m_w * exp(+I*k_w*_l_w);
+    M(3,1) = -k_w/_m_w * exp(-I*k_w*_l_w);
+    M(3,2) = -k_b/_m_b * exp(+I*k_b*_l_w);
+    M(3,3) = +k_b/_m_b * exp(-I*k_b*_l_w);
+
+    return M;
+}
+
+/**
+ * \brief calculates the uncorrelated one particle wavefunction
+ *
+ * \param[in] E Energy [J]
+ */
+std::valarray<double> SchroedingerSolverKronigPenney::get_wavefunction(const double E) const
+{
+    const auto nz = _z.size();
+    std::valarray<double> psi(nz); // wavefunction
+
+    // Normalisation constants
+    const auto I = std::complex<double>(0,1);
+
+    // Find local wave vector at this energy
+    const auto k_w = sqrt(2.0*_m_w*E)/hBar; // wave vector in the well
+    const auto k_b = sqrt(2.0*_m_b*std::complex<double>(E-_V0,0))/hBar; // Wave vector in barrier
+
+    const auto M = get_matching_matrix(E); 
+
+    arma::cx_vec rhs(4);
+    rhs(0) = 0;
+    rhs(1) = 0;
+    rhs(2) = 0;
+    rhs(3) = 1;
+
+    arma::cx_vec coeffs = arma::solve(M, rhs, "std");
+
+    const auto A = coeffs(0);
+    const auto B = coeffs(1);
+    const auto C = coeffs(2);
+    const auto D = coeffs(3);
+
+    for (unsigned int i_z=0; i_z<nz; ++i_z)
+    {
+        const auto z = _z[i_z];
+        const auto P = (A+B)*cos(k_w*z) + I*(A-B)*sin(k_w*z);
+        const auto Q = (C+D)*cos(k_b*z) + I*(C-D)*sin(k_b*z);
+
+        if (_V[i_z] > 0)
+            psi[i_z] = 1 + abs(Q) * 0;
+        else
+            psi[i_z] = 1 + abs(P) * 0;
+    }
+
+    return psi;
+}
+
+/**
+ * \brief Test how well matched the system equations are for a given energy
+ *
+ * \param[in] energy Local energy
+ * \param[in] solver The solver for which to evaluate parameters
+ *
+ * \details This gives zero for a perfect match
+ */
+double SchroedingerSolverKronigPenney::test_matching(double  energy,
+                                                     void   *params)
+{
+    const auto *se = reinterpret_cast<SchroedingerSolverKronigPenney *>(params);
+
+    const auto lhs = se->get_lhs(energy);
+    const auto rhs = se->get_rhs();
+
+    // Now evaluate the match between the left and right-hand sides
+    const auto match = lhs - rhs;
+
+    return match;
+}
+
+void SchroedingerSolverKronigPenney::calculate()
+{
+    gsl_function F;
+    F.function  = &test_matching;
+    F.params    = this;
+    auto solver = gsl_root_fsolver_alloc(gsl_root_fsolver_brent);
+
+    const auto dx=1e-3*e; // arbitrarily small energy increment---0.1meV
+
+    auto Elo=dx;    // first energy estimate
+
+    for (unsigned int ist=0; ist < _nst_max; ++ist)
+    {
+        // Shift the lower estimate up past the last state we found
+        if(ist > 0)
+            Elo = _solutions[ist-1].get_energy() + dx;
+
+        // Value for y=f(x) at bottom of search range
+        const double y1 = GSL_FN_EVAL(&F,Elo);
+
+        // Find the range in which the solution lies by incrementing the
+        // upper limit of the search range until the function changes sign.
+        // Since this coarse search can require many iterations, we keep the
+        // lower limit fixed to minimise the amount of computation at each step.
+        // The Brent algorithm is extremely fast, so it really doesn't matter that
+        // the range we find here is large.
+        //
+        // Note the end stop to prevent infinite loop in absence of solution
+        //
+        // TODO: Make the cut-off configurable
+        double y2 = y1;
+        double Ehi = Elo;
+        do
+        {
+            Ehi+=dx;
+            y2=GSL_FN_EVAL(&F, Ehi);
+        }while((y1*y2>0)&&(Ehi<100 * _V0));
+
+        auto E = (Elo + Ehi) / 2.0; // Initial estimate of energy of state
+        gsl_root_fsolver_set(solver, &F, Elo, Ehi);
+        int status = 0;
+
+        // Improve the estimate of solution using the Brent algorithm
+        // until we hit a desired level of precision
+        do
+        {
+            status = gsl_root_fsolver_iterate(solver);
+            E   = gsl_root_fsolver_root(solver);
+            Elo = gsl_root_fsolver_x_lower(solver);
+            Ehi = gsl_root_fsolver_x_upper(solver);
+            status = gsl_root_test_interval(Elo, Ehi, 1e-9*e, 0.000001);
+        }while(status == GSL_CONTINUE);
+
+        // Stop if we've exceeded the cut-off energy
+        if(_E_cutoff_set && gsl_fcmp(E, _E_cutoff, e*1e-12) == 1)
+            break;
+
+        const auto psi = get_wavefunction(E);
+        _solutions.push_back(Eigenstate(E, _z, psi));
+    }
+
+    gsl_root_fsolver_free(solver);
+}
+} // namespace
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99 :
