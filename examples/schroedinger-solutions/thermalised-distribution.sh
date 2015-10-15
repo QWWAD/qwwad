b8c6fdbb454913dd7665cb8a24375c577da2c352
Alex Valavanis <valavanisalex@gmail.com>
2015-10-15 19:06:49 +0100

    ex/ss: Migrate to new naming scheme

--- examples/schroedinger-solutions/thermalised-distribution.sh
+++ examples/schroedinger-solutions/thermalised-distribution.sh
@@ -30,19 +30,20 @@ set -e
 outfile=thermalised-distribution.dat
 rm -f Ef-T*.r N.r FD*.r N?-T.r $outfile
 
+# Set global options
+export QWWAD_ALPHA=0.701 # Nonparabolicity parameter for GaAs [1/eV]
+
 # Solve single quantum well
-efiw --width 200 --nst 6 --alpha 0.701
+qwwad_ef_infinite_well --wellwidth 200 --nst 6
 
 # Set the global population
 N=10
-
-alpha=0.701 # Nonparabolicity parameter for GaAs [1/eV]
 
 # Loop for different temperatures
 for T in `seq 10 10 1000`
 do
     # Calculate the global Fermi energy for the system
-    sbp --global-population $N --Te $T --alpha $alpha
+    qwwad_fermi_distribution --global-population $N --Te $T
 
     # Get the populations of each state from the data file 
     N1=`awk '/1\t/{printf("%8.3f\n", $2)}' N-out.r`
