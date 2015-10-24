24cc8dc3470acf194eafb954375b71a3d5c85375
Alex Valavanis <valavanisalex@gmail.com>
2015-10-24 12:39:07 +0100

    ex/ns: Name migration and tidy script

--- examples/numerical-solutions/multiple-qw-100-100.sh
+++ examples/numerical-solutions/multiple-qw-100-100.sh
@@ -9,8 +9,9 @@ set -e
 #   P. Harrison and A. Valavanis, Quantum Wells, Wires and Dots, 4th ed.
 #    Chichester, U.K.: J. Wiley, 2015, ch.2
 #
-# (c) Copyright 1996-2014
+# (c) Copyright 1996-2015
 #     Alex Valavanis <a.valavanis@leeds.ac.uk>
+#     Paul Harrison  <p.harrison@leeds.ac.uk>
 #
 # QWWAD is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -31,40 +32,38 @@ rm -f $outfile
 
 # Calculate conduction band barrier height for GaAs/Ga(1-x)Al(x)As
 # Use V=0.67*1247*x, keep x=0.2
-V=334.196
+export QWWAD_BARRIERPOTENTIAL=334.196
 
 # Calculate bulk effective mass of electron in Ga(1-x)Al(x)As
 # Use MB=0.067+0.083*x, keep x=0.2
-MB=0.1002
+export QWWAD_BARRIERMASS=0.1002
 
 # Define well and barrier widths here
-LW=100
-LB=100
-N=4
+export QWWAD_WELLWIDTH=100
+export QWWAD_BARRIERWIDTH=100
 
 # Write first barrier and initiate file
-echo 100 0.4 0.0 > s.r		
+cat > s.r << EOF
+$QWWAD_BARRIERWIDTH 0.4 0.0
+EOF
 
-# Could only think of an awk script as a replacement `for(i=1;i<N-1;i++)'
-# loop---I'm sure you must be able to do this within `/bin/sh'
-echo $N $LW $LB | awk '{
-Nwells=$1; LW=$2; LB=$3;
-while(Nwells-->1)
-    {
-        printf("%i 0.0 0.0\n",LW)
-        printf("%i 0.4 0.0\n",LB)
-    }
-}' >> s.r
-		 
-# Write last well and barrier
-echo $LW 0.0 0.0 >> s.r
-echo 100 0.4 0.0 >> s.r
+# Now append a set of wells and barriers
+for i in `seq 1 4`; do
 
-find_heterostructure --nz-1per 3000 # generate alloy concentration as a function of z
-efxv			# generate potential data
+cat >> s.r << EOF
+$QWWAD_WELLWIDTH    0.0 0.0
+$QWWAD_BARRIERWIDTH 0.4 0.0
+EOF
 
-efss --nst-max 1
-wfplot --plot-wf --plot-file $outfile
+done
+
+# Generate a mesh and find band parameters
+qwwad_mesh
+qwwad_ef_band_edge --bandedgepotentialfile v.r
+
+# Find the ground state numerically and output a plottable file
+qwwad_ef_generic --nstmax 1
+qwwad_ef_plot --style wf --plotfile $outfile
 
 cat << EOF
 Results have been written to $outfile in the format:
@@ -81,7 +80,7 @@ Results have been written to $outfile in the format:
 
 This script is part of the QWWAD software suite.
 
-(c) Copyright 1996-2014
+(c) Copyright 1996-2015
     Alex Valavanis <a.valavanis@leeds.ac.uk>
     Paul Harrison  <p.harrison@leeds.ac.uk>
 
