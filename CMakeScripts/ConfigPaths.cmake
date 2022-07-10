cec7bf07ae267d33b771ea66bb077a1809ba7108
Alex Valavanis <valavanisalex@gmail.com>
2022-07-10 11:55:47 +0100

    Fix header installation

--- CMakeScripts/ConfigPaths.cmake
+++ CMakeScripts/ConfigPaths.cmake
@@ -13,6 +13,6 @@ endif()
 set(QWWAD_SHARE_INSTALL "${SHARE_INSTALL}/qwwad")
 mark_as_advanced(QWWAD_SHARE_INSTALL)
 
-set(INSTALL_LIB_DIR  "${CMAKE_INSTALL_PREFIX}/lib"     CACHE PATH "Installation directory for libraries")
-set(INSTALL_INC_DIR  "${CMAKE_INSTALL_PREFIX}/include" CACHE PATH "Installation directory for headers")
+#set(INSTALL_LIB_DIR  "${CMAKE_INSTALL_PREFIX}/lib"     CACHE PATH "Installation directory for libraries")
+#set(INSTALL_INC_DIR  "${CMAKE_INSTALL_PREFIX}/include" CACHE PATH "Installation directory for headers")
 add_definitions(-DQWWAD_PKGDATADIR="${CMAKE_INSTALL_PREFIX}/${QWWAD_SHARE_INSTALL}")
